4f5faa0979d07649ea81c9c02c4f6c0f86485ea0
Simon Maurer <simon.maurer@humdek.unibe.ch>
2018-07-19 11:40:58 +0200

    remove old templates taht do not work anymore

--- component/style/tpl_alert.php
+++
@@ -1,5 +0,0 @@
-<div class="container<?php echo $fluid; ?>">
-    <div class="alert alert-<?php echo $level; ?>" role="alert">
-        <?php echo $content; ?>
-    </div>
-</div>
--- component/style/tpl_well.php
+++
@@ -1,8 +0,0 @@
-<div class="container<?php echo $fluid; ?> my-3">
-    <div class="card">
-        <div class="card-body">
-            <h1><?php echo $title; ?></h1>
-            <?php echo $content; ?>
-        </div>
-    </div>
-</div>
