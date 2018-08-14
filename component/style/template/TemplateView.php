90b595ad1189e200d8c79c76f4f7a7533cf77ff0
Simon Maurer <simon.maurer@humdek.unibe.ch>
2018-08-14 16:37:26 +0200

    allow to create a simple component based on a custom template

---
+++ component/style/template/TemplateView.php
@@ -0,0 +1,36 @@
+<?php
+require_once __DIR__ . "/../../BaseView.php";
+
+/**
+ * The view class of the template style component.
+ * A template style supports the following fields:
+ *  'path':     The path to the template to display.
+ *  'fields':   The fields used in the template.
+ */
+class TemplateView extends BaseView
+{
+    /* Constructors ***********************************************************/
+
+    /**
+     * The constructor.
+     *
+     * @param object $model
+     *  The model instance of the component.
+     */
+    public function __construct($model)
+    {
+        parent::__construct($model);
+    }
+
+    /* Public Methods *********************************************************/
+
+    /**
+     * Render the style view.
+     */
+    public function output_content()
+    {
+        $fields = $this->model->get_db_field("fields");
+        require $this->model->get_db_field("path");
+    }
+}
+?>
