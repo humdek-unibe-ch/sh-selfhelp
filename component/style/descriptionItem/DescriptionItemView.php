cb6d3c235eba0f025c188c8eada68e05669d6022
Simon Maurer <simon.maurer@humdek.unibe.ch>
2018-08-17 12:19:02 +0200

    db-fields as private properties

--- component/style/descriptionItem/DescriptionItemView.php
+++ component/style/descriptionItem/DescriptionItemView.php
@@ -3,13 +3,29 @@ require_once __DIR__ . "/../../BaseView.php";
 
 /**
  * The view class of the description item style component.
- * The following fields are required
- * 'title': The title of the field.
- * 'locale': The language abbreviation of the language the content of the field.
- * 'children': The components to be rendered as content of this field.
  */
 class DescriptionItemView extends BaseView
 {
+    /* Private Properties *****************************************************/
+
+    /**
+     * DB field 'title' (empty string).
+     * The title of the field. If it is left empty, the filed is not rendered.
+     */
+    private $title;
+
+    /**
+     * DB field 'locale' ('all').
+     * The language abbreviation of the language the content of the field.
+     */
+    private $locale;
+
+    /**
+     * DB style field 'alt' (empty string).
+     * The text that is displayed if no children are defined.
+     */
+    private $alt;
+
     /* Constructors ***********************************************************/
 
     /**
@@ -21,6 +37,9 @@ class DescriptionItemView extends BaseView
     public function __construct($model)
     {
         parent::__construct($model);
+        $this->title = $this->model->get_db_field("title");
+        $this->locale = $this->model->get_db_field("locale", "all");
+        $this->alt = $this->model->get_db_field("alt");
     }
 
     /* Private Methods ********************************************************/
@@ -31,7 +50,7 @@ class DescriptionItemView extends BaseView
             $this->output_children();
         else
         {
-            $na = $this->model->get_db_field("alt");
+            $na = $this->alt;
             require __DIR__ . "/tpl_item_na.php";
         }
     }
@@ -43,9 +62,8 @@ class DescriptionItemView extends BaseView
      */
     public function output_content()
     {
+        if($this->title == "") return;
         $border = "border-top";
-        $name = $this->model->get_db_field("title");
-        $locale = $this->model->get_db_field("locale");
         require __DIR__ . "/tpl_item.php";
     }
 }
--- component/style/descriptionItem/tpl_item.php
+++ component/style/descriptionItem/tpl_item.php
@@ -1,4 +1,4 @@
 <div class="mb-2 <?php echo $border; ?>">
-    <div class="d-flex"><strong><?php echo $name; ?></strong> <small class="ml-auto"><?php echo $locale; ?></small></div>
+    <div class="d-flex"><strong><?php echo $this->title; ?></strong> <small class="ml-auto"><?php echo $this->locale; ?></small></div>
     <div><?php $this->output_field_content(); ?></div>
 </div>
