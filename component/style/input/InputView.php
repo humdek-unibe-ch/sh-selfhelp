4a61101aa36d8ccd9af9dc3c0132365b6986e9bb
Simon Maurer <simon.maurer@humdek.unibe.ch>
2018-08-17 11:17:17 +0200

    fix checkbox and make db fields private properties

--- component/style/input/InputView.php
+++ component/style/input/InputView.php
@@ -4,13 +4,29 @@ require_once __DIR__ . "/../../BaseView.php";
 /**
  * The view class of the input item style component. This component renders an
  * input form field.
- * The following fields are required:
- *  'text': The default value.
- *  'name': The name of the input field.
- *  'type': The type of the input field.
  */
 class InputView extends BaseView
 {
+    /* Private Properties *****************************************************/
+
+    /**
+     * DB field 'text' (empty string).
+     * The default value of the input field.
+     */
+    private $value;
+
+    /**
+     * DB field 'type' ('text').
+     * The type of the input field.
+     */
+    private $type;
+
+    /**
+     * DB field 'name' (empty string).
+     * The name of the input field.
+     */
+    private $name;
+
     /* Constructors ***********************************************************/
 
     /**
@@ -22,6 +38,9 @@ class InputView extends BaseView
     public function __construct($model)
     {
         parent::__construct($model);
+        $this->value = $this->model->get_db_field("text");
+        $this->type = $this->model->get_db_field("type", "text");
+        $this->name = $this->model->get_db_field("name");
     }
 
     /* Public Methods *********************************************************/
@@ -31,9 +50,11 @@ class InputView extends BaseView
      */
     public function output_content()
     {
-        $text = $this->model->get_db_field("text");
-        $type = $this->model->get_db_field("type");
-        $name = $this->model->get_db_field("name");
+        $css = "form-control";
+        if($this->type == "checkbox") $css = "";
+        $checked = "";
+        if($this->type == "checkbox" && $this->value == "1")
+            $checked = "checked";
         require __DIR__ . "/tpl_input.php";
     }
 }
--- component/style/input/tpl_input.php
+++ component/style/input/tpl_input.php
@@ -1,1 +1,1 @@
-<input class="form-control" type="<?php echo $type; ?>" name="<?php echo $name; ?>" value="<?php echo $text; ?>">
+<input class="<?php echo $css; ?>" type="<?php echo $this->type; ?>" name="<?php echo $this->name; ?>" value="<?php echo $this->value; ?>" <?php echo $checked; ?>>
