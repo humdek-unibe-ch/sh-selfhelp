853aa387dd3f3812b5424d27afcec66c2d0f3944
Simon Maurer <simon.maurer@humdek.unibe.ch>
2018-08-17 15:41:04 +0200

    profile: db fields as private properties

--- component/style/profile/ProfileView.php
+++ component/style/profile/ProfileView.php
@@ -9,8 +9,89 @@ class ProfileView extends BaseView
 {
     /* Private Properties******************************************************/
 
-    private $alert_fail;
-    private $alert_success;
+    /**
+     * DB field 'alert_pw_fail' (empty string).
+     * The alert string when the password change fails.
+     */
+    private $alert_pw_fail;
+
+    /**
+     * DB field 'alert_pw_success' (empty string).
+     * The alert string when the password change succeeds.
+     */
+    private $alert_pw_success;
+
+    /**
+     * DB field 'alert_del_fail' (empty string).
+     * The alert string when the user deletion fails.
+     */
+    private $alert_del_fail;
+
+    /**
+     * DB field 'alert_del_success' (empty string).
+     * The alert string when the user deletion succeeds.
+     */
+    private $alert_del_success;
+
+    /**
+     * DB field 'pw_change_title' (empty string).
+     * The title of the password change card.
+     */
+    private $pw_change_title;
+
+    /**
+     * DB field 'pw_label' (empty string).
+     * The placeholder of the password input field.
+     */
+    private $pw_label;
+
+    /**
+     * DB field 'pw_confirm_label' (empty string).
+     * The placeholder of the password confirmation input field.
+     */
+    private $pw_confirm_label;
+
+    /**
+     * DB field 'pw_change_action_label' (empty string).
+     * The label of the password change submit button.
+     */
+    private $pw_change_action_label;
+
+    /**
+     * DB field 'user_label' (empty string).
+     * The label of the email confirmation input field.
+     */
+    private $user_label;
+
+    /**
+     * DB field 'delete_title' (empty string).
+     * The title of the delete user card.
+     */
+    private $delete_title;
+
+    /**
+     * DB field 'delete_label' (empty string).
+     * The lable of the delete user button.
+     */
+    private $delete_label;
+
+    /**
+     * DB field 'delete_content' (empty string).
+     * The description of the delete card body.
+     */
+    private $delete_content;
+
+    /**
+     * DB field 'delete_confirm_label' (empty string).
+     * The label of the delete user submit button.
+     */
+    private $delete_confirm_label;
+
+    /**
+     * DB field 'delete_confirm_content' (empty string).
+     * The content of the delete user confirmation box.
+     */
+    private $delete_confirm_content;
 
     /* Constructors ***********************************************************/
 
@@ -25,14 +106,29 @@ class ProfileView extends BaseView
     public function __construct($model, $controller)
     {
         parent::__construct($model, $controller);
+        $this->alert_pw_fail = $this->model->get_db_field('alert_pw_fail');
+        $this->alert_pw_success = $this->model->get_db_field('alert_pw_success');
+        $this->alert_del_fail = $this->model->get_db_field('alert_del_fail');
+        $this->alert_del_success = $this->model->get_db_field('alert_del_success');
+        $this->pw_change_title = $this->model->get_db_field('pw_change_title');
+        $this->pw_label = $this->model->get_db_field('pw_label');
+        $this->pw_confirm_label = $this->model->get_db_field('pw_confirm_label');
+        $this->pw_change_action_label = $this->model->get_db_field('pw_change_action_label');
+        $this->user_label = $this->model->get_db_field('user_label');
+        $this->delete_title = $this->model->get_db_field('delete_title');
+        $this->delete_label = $this->model->get_db_field('delete_label');
+        $this->delete_content = $this->model->get_db_field('delete_content');
+        $this->delete_confirm_label = $this->model->get_db_field('delete_confirm_label');
+        $this->delete_confirm_content = $this->model->get_db_field('delete_confirm_content');
+
         $this->add_alert_component("danger", "alert_pw_fail",
-            $this->model->get_db_field('alert_pw_fail'));
+            $this->alert_pw_fail);
         $this->add_alert_component("success", "alert_pw_success",
-            $this->model->get_db_field('alert_pw_success'));
+            $this->alert_pw_success);
         $this->add_alert_component("danger", "alert_del_fail",
-            $this->model->get_db_field('alert_del_fail'));
+            $this->alert_del_fail);
         $this->add_alert_component("success", "alert_del_success",
-            $this->model->get_db_field('alert_del_success'));
+            $this->alert_del_success);
     }
 
     /* Private Methods ********************************************************/
@@ -105,17 +201,6 @@ class ProfileView extends BaseView
     public function output_content()
     {
         $url = $this->model->get_link_url('profile');
-        $pw_title = $this->model->get_db_field('pw_change_title');
-        $pw_label = $this->model->get_db_field('pw_label');
-        $pw_confirm_label = $this->model->get_db_field('pw_confirm_label');
-        $pw_change_label = $this->model->get_db_field('pw_change_action_label');
-        $email_label = $this->model->get_db_field('user_label');
-        $delete_title = $this->model->get_db_field('delete_title');
-        $delete_label = $this->model->get_db_field('delete_label');
-        $delete_content = $this->model->get_db_field('delete_content');
-        $delete_confirm_label = $this->model->get_db_field('delete_confirm_label');
-        $delete_cancel_label = $this->model->get_db_field('delete_cancel_label');
-        $delete_confirm_content = $this->model->get_db_field('delete_confirm_content');
         require __DIR__ . "/tpl_profile.php";
     }
 }
--- component/style/profile/tpl_profile.php
+++ component/style/profile/tpl_profile.php
@@ -4,17 +4,17 @@
             <?php $this->output_alert_pw_change(); ?>
             <div class="card">
                 <div class="card-header">
-                    <h5 class="m-0"><?php echo $pw_title; ?></h5>
+                    <h5 class="m-0"><?php echo $this->pw_change_title; ?></h5>
                 </div>
                 <div class="card-body">
                     <form action="<?php echo $url; ?>" method="post">
                         <div class="form-group">
-                            <input type="password" class="form-control" name="password" placeholder="<?php echo $pw_label; ?>" required>
+                            <input type="password" class="form-control" name="password" placeholder="<?php echo $this->pw_label; ?>" required>
                         </div>
                         <div class="form-group">
-                            <input type="password" class="form-control" name="verification" placeholder="<?php echo $pw_confirm_label; ?>" required>
+                            <input type="password" class="form-control" name="verification" placeholder="<?php echo $this->pw_confirm_label; ?>" required>
                         </div>
-                        <button type="submit" class="btn btn-primary"><?php echo $pw_change_label; ?></button>
+                        <button type="submit" class="btn btn-primary"><?php echo $this->pw_change_action_label; ?></button>
                     </form>
                 </div>
             </div>
@@ -23,24 +23,24 @@
             <?php $this->output_alert_delete(); ?>
             <div class="card danger">
                 <div class="card-header">
-                    <h5 class="m-0"><?php echo $delete_title; ?></h5>
+                    <h5 class="m-0"><?php echo $this->delete_title; ?></h5>
                 </div>
                 <div class="card-body">
-                    <?php echo $delete_content; ?>
+                    <?php echo $this->delete_content; ?>
                     <div class="mt-3">
                         <button class="btn btn-danger" data-toggle="collapse" data-target="#confirmation" aria-expanded="false" aria-controls="confirmation">
-                            <?php echo $delete_label; ?>
+                            <?php echo $this->delete_label; ?>
                         </button>
                     </div>
                     <div class="collapse" id="confirmation">
                         <div class="card card-body">
-                            <?php echo $delete_confirm_content; ?>
+                            <?php echo $this->delete_confirm_content; ?>
                             <form method="post">
                                 <div class="form-group">
-                                    <input type="email" class="form-control" name="email" placeholder="<?php echo $email_label; ?>" required>
+                                    <input type="email" class="form-control" name="email" placeholder="<?php echo $this->user_label; ?>" required>
                                 </div>
                                 <button class="btn btn-danger" >
-                                    <?php echo $delete_confirm_label; ?>
+                                    <?php echo $this->delete_confirm_label; ?>
                                 </button>
                             </form>
                         </div>
