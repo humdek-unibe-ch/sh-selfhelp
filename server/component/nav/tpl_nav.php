6715172aa525d81d1d617346c61ad68c6db1a7d5
Simon Maurer <simon.maurer@humdek.unibe.ch>
2018-10-01 13:30:12 +0200

    use fontawesome instead of bs native because of xss

--- server/component/nav/tpl_nav.php
+++ server/component/nav/tpl_nav.php
@@ -4,7 +4,7 @@
             <?php echo $home; ?>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <i class="fas fa-bars"></i>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
