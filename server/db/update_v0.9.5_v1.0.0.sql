29ebb233bb902ebc785cdf04eab79c5f3db1f8af
Simon Maurer <simon.maurer@humdek.unibe.ch>
2019-01-09 14:03:30 +0100

    fix typos (close #89)

--- server/db/update_v0.9.5_v1.0.0.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `fields` (`id`, `name`, `id_type`, `display`) VALUES (NULL, 'css_nav', '0000000001', '0')
-INSERT INTO `styles_fields` (`id_styles`, `id_fields`, `default_value`) VALUES ('0000000033', '0000000089', NULL);
-
-UPDATE `fields` SET `display` = '1' WHERE `fields`.`id` = 0000000053;
-UPDATE `fields` SET `display` = '1' WHERE `fields`.`id` = 0000000071;
--- server/email/activate_de-CH.php
+++ server/email/activate_de-CH.php
@@ -1,6 +1,6 @@
 Guten Tag
 
-Um Ihre Email Adresse zu verivizieren und Ihren <?php echo $_SESSION['project']; ?> Account zu aktivieren klicken Sie bitte auf den untenstehenden Link.
+Um Ihre Email Adresse zu verifizieren und Ihren <?php echo $_SESSION['project']; ?> Account zu aktivieren klicken Sie bitte auf den untenstehenden Link.
 
 <?php echo $url; ?>
 
