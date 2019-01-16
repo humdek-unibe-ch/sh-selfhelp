dd959b8a298c42c04d29eb95da179181c8270001
Simon Maurer <simon.maurer@humdek.unibe.ch>
2019-01-16 14:23:38 +0100

    new audio style (close #116)

---
+++ server/component/style/audio/AudioView.php
@@ -0,0 +1,69 @@
+<?php
+require_once __DIR__ . "/../StyleView.php";
+
+/**
+ * The view class of the audio style component.
+ */
+class AudioView extends StyleView
+{
+    /* Private Properties *****************************************************/
+
+    /**
+     * DB field 'alt' (empty string)
+     * The text to be rendered if audio playback is not supported by the
+     * browser.
+     */
+    private $alt;
+
+    /**
+     * DB field 'sources' (empty string)
+     * A list of audio sources. This is an array of json objects where each
+     * object has the fileds 'source' and 'type'.
+     * If this is not set, the audio component is not rendered.
+     */
+    private $sources;
+
+    /* Constructors ***********************************************************/
+
+    /**
+     * The constructor.
+     *
+     * @param object $model
+     *  The model instance of the component.
+     */
+    public function __construct($model)
+    {
+        parent::__construct($model);
+        $this->alt = $this->model->get_db_field("alt");
+        $this->sources = $this->model->get_db_field("sources");
+    }
+
+    /* Private Methods ********************************************************/
+
+    /**
+     * Render all audio sources.
+     */
+    private function output_audio_sources()
+    {
+        if(!is_array($this->sources)) return;
+        foreach($this->sources as $source)
+        {
+            if(!isset($source["source"]) || !isset($source["type"])) continue;
+            $url = ASSET_PATH . '/' . $source["source"];
+            $type = $source["type"];
+            require __DIR__ . "/tpl_audio_source.php";
+        }
+    }
+
+    /* Public Methods *********************************************************/
+
+    /**
+     * Render the style view.
+     */
+    public function output_content()
+    {
+        if($this->sources == "") return;
+        require __DIR__ . "/tpl_audio.php";
+    }
+}
+?>
---
+++ server/component/style/audio/tpl_audio.php
@@ -0,0 +1,4 @@
+<audio class="<?php echo $this->css; ?>" controls>
+    <?php $this->output_audio_sources(); ?>
+    <?php echo $this->alt; ?>
+</audio>
