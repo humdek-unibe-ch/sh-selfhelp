dd1ab2d2a8d52aadf5585e2dcd2406b3642f1c8a
Simon Maurer <simon.maurer@humdek.unibe.ch>
2019-01-22 12:22:09 +0100

    describe the keywords that can be used in the email CMS

--- server/component/email/tpl_intro.php
+++ server/component/email/tpl_intro.php
@@ -1,4 +1,14 @@
 <div class="jumbotron">
     <h1>Manage Emails</h1>
     <p>Change the content of emails that are sent to users.</p>
+    <p>The following keywords can be used to create dynamic content:</p>
+    <ul class="">
+        <li><code>@project</code> will be replaced by the project name (the title of the page <code>home</code>).</li>
+        <li><code>@link</code> will be replaced by a link that is specific to the email it is used in:</li>
+        <ul>
+            <li>In <code>email_activate</code> the activation link is generated.</li>
+            <li>In <code>email_reset</code> the link to reset the password is generated.</li>
+            <li>In <code>email_reminder</code> the link to the project home is generated.</li>
+        </ul>
+    </ul>
 </div>
