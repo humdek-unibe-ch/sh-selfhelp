c4ffdd6ff99dab32a2a1b69cc3262fd24c99591b
Simon Maurer <simon.maurer@humdek.unibe.ch>
2019-01-22 11:28:48 +0100

    warning to fix db access rights before update

--- server/db/update_v0.9.11_v0.9.12.sql
+++ server/db/update_v0.9.11_v0.9.12.sql
@@ -1,4 +1,4 @@
--- SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'remember to update the update permission for the chat field is_new';
+SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'remember to update the update permission for the chat field is_new';
 
 UPDATE `pages` SET `nav_position` = NULL WHERE `pages`.`id` = 0000000029;
 
