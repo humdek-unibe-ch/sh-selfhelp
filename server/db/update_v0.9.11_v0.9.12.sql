31fa66d8c33bbce9728245bcfa4fa8f73bb0f41e
Simon Maurer <simon.maurer@humdek.unibe.ch>
2019-01-23 08:57:26 +0100

    cope with acl changes for therapists

--- server/db/update_v0.9.11_v0.9.12.sql
+++ server/db/update_v0.9.11_v0.9.12.sql
@@ -1,14 +1,22 @@
 SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'remember to update the update permission for the chat field is_new';
 
--- chat changes
-UPDATE `pages` SET `nav_position` = NULL WHERE `pages`.`id` = 0000000029;
-
-ALTER TABLE `chat` ADD `is_new` TINYINT(0) NOT NULL DEFAULT '1' AFTER `timestamp`;
-
+-- chat changes (ACL and Symbol position)
 INSERT INTO `pageType` (`id`, `name`) VALUES (NULL, 'chat');
 SET @id_page_type_chat = LAST_INSERT_ID();
-UPDATE `pages` SET `id_type` = @id_page_type_chat WHERE `pages`.`id` = 0000000029;
+UPDATE `pages` SET `nav_position` = NULL WHERE `pages`.`keyword` = 'contact';
+UPDATE `pages` SET `id_type` = @id_page_type_chat WHERE `pages`.`keyword` = 'contact';
 
+-- allow POST on all experimenter pages and on home
+UPDATE `pages` SET `protocol` = 'GET|POST' WHERE `pages`.`id` = 0000000002;
+UPDATE `pages` SET `protocol` = 'GET|POST' WHERE `pages`.`id_type` = 0000000003;
+
+-- change group name
+UPDATE `groups` SET `name` = 'therapist' WHERE `groups`.`id` = 0000000002;
+
+-- chat changes for new messages indicator
+ALTER TABLE `chat` ADD `is_new` TINYINT(0) NOT NULL DEFAULT '1' AFTER `timestamp`;
+
+-- chat changes for chat groups
 CREATE TABLE `chatRoom` (
   `id` int(10) UNSIGNED ZEROFILL NOT NULL,
   `name` varchar(100) NOT NULL
@@ -35,10 +43,3 @@ ALTER TABLE `chatRoom_users`
 
 ALTER TABLE `chat` ADD `id_rcv_grp` INT UNSIGNED ZEROFILL NULL DEFAULT NULL AFTER `id_rcv`, ADD INDEX (`id_rcv_grp`);
 ALTER TABLE `chat` ADD CONSTRAINT `fk_chat_id_rcv_grp` FOREIGN KEY (`id_rcv_grp`) REFERENCES `chatRoom`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- allow POST on all experimenter pages and on home
-UPDATE `pages` SET `protocol` = 'GET|POST' WHERE `pages`.`id` = 0000000002;
-UPDATE `pages` SET `protocol` = 'GET|POST' WHERE `pages`.`id_type` = 0000000003;
-
--- change group name
-UPDATE `groups` SET `name` = 'therapist' WHERE `groups`.`id` = 0000000002;
