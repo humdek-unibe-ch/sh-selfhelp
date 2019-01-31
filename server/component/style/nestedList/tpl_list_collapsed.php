8e6e3ca744c7e02b683ca2bce56ec41b031d0e53
Simon Maurer <simon.maurer@humdek.unibe.ch>
2019-01-31 14:16:56 +0100

    fix add custom css fields on collapible nested list

--- server/component/style/nestedList/tpl_list_collapsed.php
+++ server/component/style/nestedList/tpl_list_collapsed.php
@@ -1,6 +1,6 @@
 <button class="nested-list-menu-responsive collapsed d-md-none d-auto btn btn-secondary w-100 rounded-0 text-truncate <?php echo $this->css; ?>" type="button">
     <i class="fas fa-bars mr-2"></i><?php echo $title; ?>
 </button>
-<div class="list-group collapse d-md-block nested-list-menu-collapsible border border-secondary rounded-bottom border-top-0 p-2 w-100">
+    <div class="list-group collapse d-md-block nested-list-menu-collapsible border border-secondary rounded-bottom border-top-0 p-2 w-100 <?php echo $this->css; ?>">
     <?php $this->output_list(); ?>
 </div>
