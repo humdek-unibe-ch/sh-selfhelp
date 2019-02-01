c755b3e1733f0e24946e8d0b58e92555e8f8d6ae
Simon Maurer <simon.maurer@humdek.unibe.ch>
2019-02-01 13:04:19 +0100

    make page and section navigation sticky

--- server/component/cms/tpl_main.php
+++ server/component/cms/tpl_main.php
@@ -1,8 +1,10 @@
 <div class="container-fluid mt-3">
     <div class="row">
         <div class="col-auto">
-            <?php $this->output_create_new_button(); ?>
-            <?php $this->output_lists(); ?>
+            <div class="sticky-top">
+                <?php $this->output_create_new_button(); ?>
+                <?php $this->output_lists(); ?>
+            </div>
         </div>
         <div class="col">
             <?php $this->output_page_content(); ?>
