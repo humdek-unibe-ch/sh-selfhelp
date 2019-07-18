fd76f5c52cc1328f1ba85f95323ef6233ebf5f0a
Simon Maurer <simon.maurer@humdek.unibe.ch>
2019-07-18 09:09:04 +0200

    update bootstrap

--- server/page/BasePage.php
+++ server/page/BasePage.php
@@ -108,8 +108,7 @@ abstract class BasePage
         );
         $this->js_includes = array(
             "/js/ext/jquery.min.js",
-            "/js/ext/popper.min.js",
-            "/js/ext/bootstrap.min.js",
+            "/js/ext/bootstrap.bundle.min.js",
         );
         if(DEBUG == 0)
         {
