1a3afcafc5a6f2987b7a6654eb4d6fdd922ccc10
Simon Maurer <simon.maurer@humdek.unibe.ch>
2019-07-19 11:06:45 +0200

    merge db script from #164

--- server/db/update_v1.0.4_v1.1.0.sql
+++ server/db/update_v1.0.4_v1.1.0.sql
@@ -68,3 +68,34 @@ SET @id_style_userProgress = LAST_INSERT_ID();
 INSERT INTO `styles_fields` (`id_styles`, `id_fields`, `default_value`, `help`) VALUES (@id_style_userProgress, (SELECT `id` FROM `fields` WHERE `name` = 'type'), NULL, '.Use the type to change the appearance of individual progress bars');
 INSERT INTO `styles_fields` (`id_styles`, `id_fields`, `default_value`, `help`) VALUES (@id_style_userProgress, (SELECT `id` FROM `fields` WHERE `name` = 'is_striped'), NULL, 'iIf set apply a stripe via CSS gradient over the progress bar’s background color.');
 INSERT INTO `styles_fields` (`id_styles`, `id_fields`, `default_value`, `help`) VALUES (@id_style_userProgress, (SELECT `id` FROM `fields` WHERE `name` = 'has_label'), NULL, 'If set display the progress in numbers ontop of the progress bar.');
+
+-- issue 164
+--
+-- Table structure for table `pages_fields`
+--
+
+CREATE TABLE `pages_fields` (
+  `id_pages` int(10) UNSIGNED ZEROFILL NOT NULL,
+  `id_fields` int(10) UNSIGNED ZEROFILL NOT NULL,
+  `default_value` varchar(100) DEFAULT NULL,
+  `help` longtext
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for table `pages_fields`
+--
+ALTER TABLE `pages_fields`
+  ADD PRIMARY KEY (`id_pages`,`id_fields`),
+  ADD KEY `id_pages` (`id_pages`),
+  ADD KEY `id_fields` (`id_fields`);
+
+--
+-- Constraints for table `pages_fields`
+--
+ALTER TABLE `pages_fields`
+  ADD CONSTRAINT `fk_page_fields_id_fields` FOREIGN KEY (`id_fields`) REFERENCES `fields` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_page_fields_id_pages` FOREIGN KEY (`id_pages`) REFERENCES `pages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+INSERT INTO `fields` (`name`, `id_type`, `display`) VALUES('description', (SELECT id FROM `fieldType` WHERE `name` = 'textarea'), 1);
+SET @id_field_description = LAST_INSERT_ID();
+INSERT INTO `pages_fields` (`id_pages`, `id_fields`, `default_value`, `help`) VALUES((SELECT id FROM `pages` WHERE `keyword` = 'home'), @id_field_description, NULL, 'A short description of the research project. This field will be used as `meta:description` in the HTML header. Some services use this tag to provide the user with information on the webpage (e.g. automatic link-replacement in messaging tools on smartphones use this description.)');
