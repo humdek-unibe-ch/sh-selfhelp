3d18dd4d971db87dd937ff77226225f7e69592ef
Simon Maurer <simon.maurer@humdek.unibe.ch>
2019-07-24 09:50:22 +0200

    set id of mermaidForm to allow to jump to the form on data submit

--- server/component/style/mermaidForm/tpl_mermaidForm.php
+++ server/component/style/mermaidForm/tpl_mermaidForm.php
@@ -1,6 +1,6 @@
 <?php $this->output_controller_alerts_fail(); ?>
 <?php $this->output_controller_alerts_success(); ?>
-<div class="mermaidForm <?php echo $this->css; ?>">
+<div id="section-<?php echo $this->id_section; ?>" class="mermaidForm <?php echo $this->css; ?>">
    <div class="mermaidCode"><?php echo $code; ?></div>
    <div class="mermaidFormEditableFields mermaidFormHidden"><?php echo $fields; ?></div>
    <div class="mermaidFormName mermaidFormHidden"><?php echo $formName; ?></div>
