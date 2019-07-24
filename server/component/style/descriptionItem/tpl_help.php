9e1c01b77ac56f029ad53c0896ebacc872829d53
Simon Maurer <simon.maurer@humdek.unibe.ch>
2019-07-24 18:14:22 +0200

    show popover on hover

--- server/component/style/descriptionItem/tpl_help.php
+++ server/component/style/descriptionItem/tpl_help.php
@@ -1,3 +1,3 @@
-<a href="#" tabindex="0" class="text-secondary ml-1" data-trigger="focus" data-container="body" data-toggle="popover" data-placement="top" data-content='<?php echo $this->help; ?>'>
+<a href="#" tabindex="0" class="text-secondary ml-1" data-trigger="hover" data-container="body" data-toggle="popover" data-placement="top" data-content='<?php echo $this->help; ?>'>
     <i class="fas fa-info-circle"></i>
 </a>
