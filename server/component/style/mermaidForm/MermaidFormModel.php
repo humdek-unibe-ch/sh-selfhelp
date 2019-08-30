3017a5d87f07427a9561c8242a4b0085a462752f
Stefan Kodzhabashev <stefan.kodzhabashev@psy.unibe.ch>
2019-08-30 13:08:10 +0200

    bug fix #185 [css display rule for select and textarea]; new feature #184 [the fiedl label is used now as a title. If it is not empty it will be always generated even if there is a value]; bugfix #182 [regex changed to simple replace of double quotes to single quotes, then the whol string is enveloped with double quotes, as addition new line is replaced by <br> in HMLT code format]

--- server/component/style/mermaidForm/MermaidFormModel.php
+++ server/component/style/mermaidForm/MermaidFormModel.php
@@ -9,96 +9,110 @@ require_once __DIR__ . "/../StyleComponent.php";
  */
 class MermaidFormModel extends FormUserInputModel
 {
-    /* Private Properties *****************************************************/
+   /* Private Properties *****************************************************/
 
-    /* Constructors ***********************************************************/
+   /* Constructors ***********************************************************/
 
-    /**
-     * The constructor fetches all session related fields from the database.
-     *
-     * @param array $services
-     *  An associative array holding the different available services. See the
-     *  class definition basepage for a list of all services.
-     * @param int $id
-     *  The section id of the navigation wrapper.
-     */
-    public function __construct($services, $id)
-    {
-        parent::__construct($services, $id);
-    }
+   /**
+    * The constructor fetches all session related fields from the database.
+    *
+    * @param array $services
+    *  An associative array holding the different available services. See the
+    *  class definition basepage for a list of all services.
+    * @param int $id
+    *  The section id of the navigation wrapper.
+    */
+   public function __construct($services, $id)
+   {
+      parent::__construct($services, $id);
+   }
 
-    /* Public Methods *********************************************************/
+   /* Public Methods *********************************************************/
 
-    /**
-     * Get fields that will be editable by the mermaid, we return a json array.
-     *
-     * @param array $children
-     *  The array of input field components
-     * @retval jsonarray  of structure
-     * name:{
-     *   value: field_value,
-     *   label: label_used_for_description
-     * }
-     */
-    public function get_user_field_names_with_values($children)
-    {
-        $arrFields = [];
-        foreach($children as $child)
-        {
-            if(is_a($child, "StyleComponent"))
-            {
-                $name = $child->get_style_instance()->get_view()
-                    ->get_name_base();
-                $value = $child->get_style_instance()->get_model()
-                    ->get_form_field_value();
-                if($this->is_cms_page()){
-                    $arrFields[$name] = array(
-                        "value" => '',
-                        "label" => ''
-                    );
-                }else{
-                    $arrFields[$name] = array(
-                        "value" => $value,
-                        "label" => ''
-                    );
-                }
-                $arrFields[$name]['label'] = $child->get_style_instance()
-                    ->get_view()->get_label();
+   /**
+    * Get fields that will be editable by the mermaid, we return a json array.
+    *
+    * @param array $children
+    *  The array of input field components
+    * @retval jsonarray  of structure
+    * name:{
+    *   value: field_value,
+    *   label: label_used_for_description
+    * }
+    */
+   public function get_user_field_names_with_values($children)
+   {
+      $arrFields = [];
+      foreach ($children as $child) {
+         if (is_a($child, "StyleComponent")) {
+            $name = $child->get_style_instance()->get_view()
+               ->get_name_base();
+            $value = $child->get_style_instance()->get_model()
+               ->get_form_field_value();
+            if ($this->is_cms_page()) {
+               $arrFields[$name] = array(
+                  "value" => '',
+                  "label" => ''
+               );
+            } else {
+               $arrFields[$name] = array(
+                  "value" => $this->replaceQuotes($value), //remove quotes here otherwose we break json encode
+                  "label" => ''
+               );
             }
-        }
-        return $arrFields;
-    }
-
-    /**
-     * Parse the code string and replace the labels with the values entered by
-     * the subject.
-     *
-     * @param array $fields
-     *  The array of input field values, see
-     *  MermaidFormModel::get_user_field_names_with_values().
-     * @param string $code
-     *  The original code defined by the experimenter
-     * @retval string
-     *  The modified code where the editable node labels were replaced by the
-     *  user input values stored in the DB.
-     */
-    public function replace_user_field_values_in_code($fields, $code)
-    {
-        if($this->is_cms_page())
-            return $code;
-        foreach($fields as $name => $field)
-        {
-            $pattern = "~" . $name . "(\[|\(\(|\(|{|>)(.*?)(\]|\)|\)\)|})~";
-            preg_match_all($pattern, $code, $matches, PREG_PATTERN_ORDER);
-            foreach($matches[0] as $match)
-            {
-                if($field['value'] == "") continue;
-                $val = preg_replace("/[^a-zA-Z0-9_-]+/", "", $field['value']);
-                $new = $name.$matches[1][0].$val.$matches[3][0];
-                $code = str_replace($match, $new, $code);
+            $arrFields[$name]['label'] = $child->get_style_instance()
+               ->get_view()->get_label();
+            if ($arrFields[$name]['label'] != '') {
+               //if label is assigne, it will be used as a title and it will be vizualized everytime, then 2 new lines
+               $arrFields[$name]['value'] = $arrFields[$name]['label'] . '&lt;br&gt;&lt;br&gt;' . ($arrFields[$name]['value'] == '' ? '...' : $arrFields[$name]['value']);
             }
-        }
-        return $code;
-    }
+         }
+      }
+      return $arrFields;
+   }
+
+   /**
+    * Parse the code string and replace all double quotes with single quotes
+    *
+    * @orig_string string 
+
+    */
+
+   public function replaceQuotes($orig_string)
+   {
+      return str_replace('&#34;', "'", $orig_string);
+   }
+
+   /**
+    * Parse the code string and replace the labels with the values entered by
+    * the subject.
+    *
+    * @param array $fields
+    *  The array of input field values, see
+    *  MermaidFormModel::get_user_field_names_with_values().
+    * @param string $code
+    *  The original code defined by the experimenter
+    * @retval string
+    *  The modified code where the editable node labels were replaced by the
+    *  user input values stored in the DB.
+    */
+   public function replace_user_field_values_in_code($fields, $code)
+   {
+      if ($this->is_cms_page())
+         return $code;
+      foreach ($fields as $name => $field) {
+         $pattern = "~" . $name . "(\[|\(\(|\(|{|>)(.*?)(\]|\)|\)\)|})~";
+         preg_match_all($pattern, $code, $matches, PREG_PATTERN_ORDER);
+         foreach ($matches[0] as $match) {
+            if ($field['value'] == "") {
+               continue;
+            }
+            $val = '"' . $this->replaceQuotes($field['value']) . '"'; //replace quotes here otherwise we break mermaid MD syntax
+            $val = str_replace("\r\n", '&lt;br&gt;', $val); //replace new line with <br> in code in order to show the new lines in Text area. Mermaid parse only <br> as new line
+            $new = $name . $matches[1][0] . $val . $matches[3][0];
+            $code = str_replace($match, $new, $code);
+         }
+      }
+      return $code;
+   }
 }
-?>
--- server/component/style/mermaidForm/js/initMermaid.js
+++ server/component/style/mermaidForm/js/initMermaid.js
@@ -1,90 +1,91 @@
 $(document).ready(function () {
-    mermaid.mermaidAPI.initialize({
-        startOnLoad:false
-    });
-    $(".mermaidForm").each(function() {
-        var $element = $(this).children('div.mermaidCode:first');
-        var formName = $(this).children('div.mermaidFormName:first').text();
-        var j_editableFields = $(this).children('div.mermaidFormEditableFields:first').text();
+   mermaid.mermaidAPI.initialize({
+      startOnLoad: false
+   });
+   $(".mermaidForm").each(function () {
+      var $element = $(this).children('div.mermaidCode:first');
+      var formName = $(this).children('div.mermaidFormName:first').text();
+      var j_editableFields = $(this).children('div.mermaidFormEditableFields:first').text();
 
-        var insertSvg = function(svgCode) {
-            var $svgCode = $(svgCode);
-            addNodeIdPrefix(formName, $svgCode);
-            $element.html($svgCode);
-            addPointerClassAndClickEvent(formName, j_editableFields);
-        };
-
-        var graph = mermaid.mermaidAPI.render('graph-' + formName, $element.text(), insertSvg);
-        growNodesToFitText($('#graph-' + formName).find('.node'));
-    });
+      var insertSvg = function (svgCode) {
+         var $svgCode = $(svgCode);
+         addNodeIdPrefix(formName, $svgCode);
+         $element.html($svgCode);
+         addPointerClassAndClickEvent(formName, j_editableFields);
+      };
+      var graph = mermaid.mermaidAPI.render('graph-' + formName, $element.text(), insertSvg);
+      growNodesToFitText($('#graph-' + formName).find('.node'));
+   });
 });
 
-function addNodeIdPrefix(formName, $svgCode){
-    //prefix each node with formName for uniquness
-    var allNodes = $svgCode.find('.node');
-    allNodes.each(function(){
-        if(!$(this).attr('id').includes(formName)){
-            $(this).attr('id', formName + '_' + $(this).attr('id'));
-        }
-    });
+function addNodeIdPrefix(formName, $svgCode) {
+   //prefix each node with formName for uniquness
+   var allNodes = $svgCode.find('.node');
+   allNodes.each(function () {
+      if (!$(this).attr('id').includes(formName)) {
+         $(this).attr('id', formName + '_' + $(this).attr('id'));
+      }
+   });
 }
 
-function growNodesToFitText($nodes){
-    // grow the bounding box of the text of a node to not crop the text.
-    $nodes.each(function(){
-        var $foreignObject = $(this).find('foreignObject:first');
-        var $div = $foreignObject.children('div:first');
-        var delta = $foreignObject.find('div:first').width()
-            - $foreignObject.attr('width');
-        $foreignObject.attr('width', parseInt($foreignObject.attr('width')) + delta);
-    });
+function growNodesToFitText($nodes) {
+   // grow the bounding box of the text of a node to not crop the text.
+   $nodes.each(function () {
+      var $foreignObject = $(this).find('foreignObject:first');
+      var $div = $foreignObject.children('div:first');
+      var delta = $foreignObject.find('div:first').width()
+         - $foreignObject.attr('width');
+      $foreignObject.attr('width', parseInt($foreignObject.attr('width')) + delta);
+   });
 }
 
-function addPointerClassAndClickEvent(formName, j_editableFields){
-    // add pointer class to editable fields
-    var editableFields = parseEditableFields(j_editableFields);
-    for (var name in editableFields){
-        $('#' + formName + '_' + name).addClass('pointer');
-        $('#' + formName + '_' + name).click((function(_name) {
-            return function() {
-                onClickToEdit($(this).attr('id'), formName, editableFields[_name]);
-            }
-        })(name));
-    }
+function addPointerClassAndClickEvent(formName, j_editableFields) {
+   // add pointer class to editable fields
+   var editableFields = parseEditableFields(j_editableFields);
+   for (var name in editableFields) {
+      $('#' + formName + '_' + name).addClass('pointer');
+      $('#' + formName + '_' + name).click((function (_name) {
+         return function () {
+            onClickToEdit($(this).attr('id'), formName, editableFields[_name]);
+         }
+      })(name));
+   }
 }
 
-function onClickToEdit(id, formName, editableField){
-    // fire modal form on clik on editable field
-    $("#" + formName).find("form input[type=text]").each(function(){
-        if(formName + "_" + $(this).attr('name') == id + "[value]") {
-            //add label as title and if it is empty put a default one
-            if(editableField.label != '')
-                $('#' + formName).find('.modal-title').html(editableField.label);
-            else
-                $(this).addClass('mb-3');
-            //add visble class to the field that the user wants to edit
-            $(this).addClass('mermaidFieldVisible');
-        }
-        else
-            $(this).prop('required', false);
-    });
-    $("#"+ formName).modal();
-    $("#"+ formName).on('hidden.bs.modal', function () {
-        //on modal close make all visible fields hidden
-        $('.mermaidFieldVisible').each(function(){
-            $(this).removeClass('mermaidFieldVisible');
-        });
+function onClickToEdit(id, formName, editableField) {
+   // fire modal form on clik on editable field
+   $("#" + formName).find(":input").each(function () {
+      if (formName + "_" + $(this).attr('name') == id + "[value]") {
+         //add label as title and if it is empty put a default one
+         if (editableField.label != '') {
+            $('#' + formName).find('.modal-title').html(editableField.label);
+         } else {
+            $(this).addClass('mb-3');
+         }
+         //add visble class to the field that the user wants to edit
+         console.log(this);
+         $(this).addClass('mermaidFieldVisible');
+      }
+      else
+         $(this).prop('required', false);
+   });
+   $("#" + formName).modal();
+   $("#" + formName).on('hidden.bs.modal', function () {
+      //on modal close make all visible fields hidden
+      $('.mermaidFieldVisible').each(function () {
+         $(this).removeClass('mermaidFieldVisible');
+      });
 
-    });
+   });
 }
 
-function parseEditableFields(j_editableFields){
-    // get editable fields and values and return them as object
-    try{
-        return JSON.parse(j_editableFields);
-    }
-    catch(e){
-        console.log(e);
-        return {};
-    }
+function parseEditableFields(j_editableFields) {
+   // get editable fields and values and return them as object
+   try {
+      return JSON.parse(j_editableFields);
+   }
+   catch (e) {
+      console.log(e);
+      return {};
+   }
 }
