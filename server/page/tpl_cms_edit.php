e5cebb7840a9a4365bb2e3d67661e38b30f4ef3e
Simon Maurer <simon.maurer@humdek.unibe.ch>
2019-09-18 12:32:50 +0200

    add a button to move back to the top of a page (close #191)

--- server/page/tpl_cms_edit.php
+++ server/page/tpl_cms_edit.php
@@ -1,1 +1,1 @@
-<div class="m-3 fixed-bottom cms-edit border rounded"><a class="text-dark" href="<?php echo $url; ?>"><i class="p-3 fas fa-edit"></i></a></div>
+<a class="text-dark" href="<?php echo $url; ?>"><i class="py-3 pl-3 fas fa-edit"></i></a>
---
+++ server/page/tpl_to_top.php
@@ -0,0 +1,1 @@
+<a class="text-dark" href="<?php echo $url; ?>"><i class="p-3 fas fa-chevron-up"></i></a>
