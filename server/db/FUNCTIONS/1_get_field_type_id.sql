c999123a67b490070a7019e4946ab37b3399e25a
Stefan Kodzhabashev <stefan.kodzhabashev@psy.unibe.ch>
2019-09-20 09:59:55 +0200

    db function get_field_type_id

---
+++ server/db/FUNCTIONS/1_get_field_type_id.sql
@@ -0,0 +1,14 @@
+DELIMITER //
+DROP FUNCTION IF EXISTS get_field_type_id //
+
+CREATE FUNCTION get_field_type_id(field_type varchar(100)) RETURNS INT
+BEGIN 
+	DECLARE field_type_id INT;    
+	select id into field_type_id
+	from fieldtype
+	where name = field_type;
+    return field_type_id;
+END
+//
+
+DELIMITER ;
