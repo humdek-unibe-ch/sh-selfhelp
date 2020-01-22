c11da55d263e0bc0c6b64668290cfc785ef98156
Stefan Kodzhabashev <stefan.kodzhabashev@psy.unibe.ch>
2020-01-22 17:48:36 +0100

    showUserInput is a datatable now. By default all functionality is stripped. If you want to add configuration you have to use css classes in the showUserInput form classes: dt-sortable - make all columsn sortable dt-searching - add the search field dt-bPaginate - add the page grouping dt-bInfo - add the info footer dt-order-0-asc dt-order-1-desc - order the desired column asc or desc dt-hide-0 - hide the desired column

--- server/component/style/showUserInput/js/showUserInput.js
+++ server/component/style/showUserInput/js/showUserInput.js
@@ -1,11 +1,59 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var $input = $('input[name="user_input_remove_id"]');
-    $('.remove-user-input-field').click(function() {
+    $('.remove-user-input-field').click(function () {
         var ids = [];
-        $(this).siblings('[id|="user-input-field"]').each(function() {
+        $(this).siblings('[id|="user-input-field"]').each(function () {
             var id = $(this).attr('id').split('-');
             ids.push(id[id.length - 1]);
         });
         $input.val(ids);
+    });
+
+    $('.showUserInput').each(function () {
+        var classes = $(this).attr('class').split(' ');
+
+        var dataTableConfig = { columnDefs: [] }; //init data table config
+
+        dataTableConfig.columnDefs.push({ orderable: classes.includes('dt-sortable'), targets: '_all' }); //sortable
+        dataTableConfig.searching = classes.includes('dt-searching'); //searching
+        dataTableConfig.bPaginate = classes.includes('dt-bPaginate'); //bPaginate
+        dataTableConfig.bInfo = classes.includes('dt-bInfo'); //bInfo
+
+        //check for ordered columns **********************************************************************************
+        // dt-order-0-asc dt-order-1-desc
+        var ordered = classes.filter(function (str) { return str.includes("dt-order"); });
+        for (let i = 0; i < ordered.length; i++) {
+            const ordClassElements = ordered[i].split('-');
+            if (ordClassElements.length == 4) {
+                //correct order pattern                
+                if (ordClassElements[2] == parseInt(ordClassElements[2], 10) && (ordClassElements[3] === 'asc' || ordClassElements[3] === 'desc')) {
+                    // check is 3 element number and 4 asc or desc
+                    if (!dataTableConfig.order) {
+                        //init order property
+                        dataTableConfig.order = [];
+                    }
+                    dataTableConfig.order.push([ordClassElements[2], ordClassElements[3]])
+                }
+            }
+        }
+
+        //check for hiiden columns **********************************************************************************
+        // dt-hide-0
+        var ordered = classes.filter(function (str) { return str.includes("dt-hide"); });
+        for (let i = 0; i < ordered.length; i++) {
+            const ordClassElements = ordered[i].split('-');
+            if (ordClassElements.length == 3) {
+                //correct order pattern                
+                if (ordClassElements[2] == parseInt(ordClassElements[2], 10)) {
+                    // check is 3 element number and 4 asc or desc
+                    dataTableConfig.columnDefs.push({
+                        "targets": [parseInt(ordClassElements[2], 10)],
+                        "visible": false
+                    })
+                }
+            }
+        }
+
+        $(this).DataTable(dataTableConfig);
     });
 });
