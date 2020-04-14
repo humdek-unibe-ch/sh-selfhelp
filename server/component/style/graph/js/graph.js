66352ba137d3d2b9f7823aaec4772ebf849df2b1
Simon Maurer <moirelein@gmail.com>
2020-04-14 15:18:37 +0200

    extend the default cb function to allow for more flexibility and to cover a wider range of graph types

--- server/component/style/graph/js/graph.js
+++ server/component/style/graph/js/graph.js
@@ -36,7 +36,7 @@ function drawGraph($div, traces, layout, config, post_process = () => {}, regist
                         if('cb' in data_source)
                             keys = data_source.cb(data.data);
                         else
-                            keys = trace_cb(data.data, data_source);
+                            keys = graphTraceCb(data.data, data_source);
 
                         if(idx === 0) {
                             Plotly.newPlot($div[0], [], layout, config);
@@ -70,15 +70,113 @@ function drawGraph($div, traces, layout, config, post_process = () => {}, regist
 
 }
 
-function trace_cb(data, data_source) {
+/**
+ * The default callback function to be appliad on a trace data set.
+ *
+ * @param array data
+ *  An array of rows where each row is an object with key value pairs where the
+ *  key is the column name.
+ * @param object data_source
+ *  This object allow sto define how the data from parameter `data` is assigned
+ *  to trace keys. The object has the following keys:
+ *  - `name<string>`: the name of the data source table as stored in the DB
+ *  - `map<object>`: the association of the data to plotly trace keys. This is
+ *    an object where each key is a dot-seperated string which will produce an
+ *    element inside the trace object accordingly. The value of a map item can
+ *    be of the following types:
+ *     - `string`: indicating the data column to be used as values
+ *     - `array`: an array of operand objects which allow to perform simple
+ *       operations on data columns and use the result as values. An operand
+ *       object must have the following keys:
+ *        - `name<string>`: the name of the data column
+ *        - `op<string>`: one of the following mathematical operations:
+ *          - `max`: returns the maximal value of the data column.
+ *          - `min`: returns the minimal value of the data column.
+ *          - `sum`: returns the sum of all values of the data column.
+ *          - `avg`: returns the average of all values of the data column.
+ *     - `object`: an object definig how to group values of one data column and
+ *       how to assign labels to each group. The object must have the following
+ *       keys:
+ *       - `name<string>`: the name of the data column.
+ *       - `op<string>`: one of the following operations to produce grouping
+ *         values:
+ *         - `count`: count the occurences of each individual value
+ *         - `sum`: accumulate distinct values (i.e. count * value)
+ *       - `labels<object>`: allows to assign labels to each value. The
+ *         following keys are expected:
+ *         - `key<string>`: a dot-seperated string which will produce an
+ *           element inside the trace object accordingly.
+ *         - `map<object>`: a list of key-value pairs where the key corresponds
+ *           to the data value to be labelled and the value is a label string.
+ * @return
+ *  A partial plotly trace objects with keys set as defined in param
+ *  `data_source`.
+ */
+function graphTraceCb(data, data_source) {
     let trace = {};
     for(let key in data_source.map) {
-        trace[key] = [];
-        data.forEach(function(item) {
-            trace[key].push(item[data_source.map[key]])
-        });
+        graphExpandDotString(trace, key, []);
+        let source = data_source.map[key];
+        if(Array.isArray(source)) {
+            // column operations
+            source.forEach(function(item) {
+                let val = 0;
+                if(item.op === "max") {
+                    val = Math.max.apply(Math,
+                        data.map(function(o) {return o[item.name]}));
+                } else if (item.op === "min") {
+                    val = Math.min.apply(Math,
+                        data.map(function(o) {return o[item.name]}));
+                } else if (item.op === "sum") {
+                    val = data.reduce((a, b) => {
+                        return a + Number(b[item.name])
+                    }, 0);
+                } else if (item.op === "avg") {
+                    val = data.reduce((a, b) => {
+                        return a + Number(b[item.name])
+                    }, 0);
+                    val /= data.length;
+                }
+                trace[key].push(val);
+            });
+        } else if(typeof source === 'object' && source !== null) {
+            let vals = {};
+            graphExpandDotString(trace, source.labels.key, []);
+            data.forEach(function(item) {
+                let val = item[source.name];
+                if(!(val in vals)) {
+                    vals[val] = trace[key].length;
+                    trace[key].push(0);
+                    trace[source.labels.key].push(source.labels.map[val]);
+                }
+                if(source.op === "count") {
+                    trace[key][vals[val]]++;
+                } else if(source.op === "sum") {
+                    trace[key][vals[val]] += val;
+                }
+            });
+        } else {
+            data.forEach(function(item) {
+                trace[key].push(item[source])
+            });
+        }
     }
+    console.log(trace);
     return trace;
+}
+
+function graphExpandDotString(obj, str, value) {
+    var items = str.split('.');
+    var ref = obj;
+
+    //  loop through all nodes, except the last one
+    for(var i = 0; i < items.length - 1; i ++)
+    {
+        ref[items[i]] = {};
+        ref = ref[items[i]]; // shift the reference to the newly created object
+    }
+
+    ref[items[items.length - 1]] = value; // apply the final value
 }
 
 
