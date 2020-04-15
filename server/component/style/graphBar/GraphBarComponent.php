c381689028a8c9f0a8f7f9722e63f2e667e60fa9
Simon Maurer <moirelein@gmail.com>
2020-04-15 17:11:25 +0200

    first commit of style graphBar (not working)

---
+++ server/component/style/graphBar/GraphBarComponent.php
@@ -0,0 +1,38 @@
+<?php
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+?>
+<?php
+require_once __DIR__ . "/../../BaseComponent.php";
+require_once __DIR__ . "/GraphBarView.php";
+require_once __DIR__ . "/GraphBarModel.php";
+
+/**
+ * A component class for a graphBar style component. This style is intended to
+ * display a Bar diagram.
+ */
+class GraphBarComponent extends BaseComponent
+{
+    /* Constructors ***********************************************************/
+
+    /**
+     * The constructor creates an instance of the Model class and the View
+     * class and passes the view instance to the constructor of the parent
+     * class.
+     *
+     * @param array $services
+     *  An associative array holding the different available services. See the
+     *  class definition basepage for a list of all services.
+     * @param int $id
+     *  The section id of this component.
+     */
+    public function __construct($services, $id)
+    {
+        $model = new GraphBarModel($services, $id);
+        $view = new GraphBarView($model);
+
+        parent::__construct($model, $view);
+    }
+}
+?>
