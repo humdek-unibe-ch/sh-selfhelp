4284ad4945dd117f7f0208d9c2ab12a6e16212ed
Simon Maurer <moirelein@gmail.com>
2020-04-17 15:10:01 +0200

    re-enable responsive graphs

--- server/component/style/graph/GraphView.php
+++ server/component/style/graph/GraphView.php
@@ -73,6 +73,12 @@ class GraphView extends StyleView
             }
             $this->layout["title"] = $this->title;
         }
+        if(!isset($this->config["responsive"])) {
+            if($this->config === "") {
+                $this->config = array();
+            }
+            $this->config["responsive"] = true;
+        }
 
         echo json_encode(array(
             "graph_type" => $this->graph_type,
