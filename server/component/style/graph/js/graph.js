9bdc9e01be26c25190f85304739634407ffa5098
Simon Maurer <moirelein@gmail.com>
2020-04-29 10:51:42 +0200

    add further trace ordering options and document code

--- server/component/style/graph/js/graph.js
+++ server/component/style/graph/js/graph.js
@@ -13,6 +13,15 @@ $(document).ready(() => {
     });
 });
 
+/**
+ * Draw a plotly graph
+ *
+ * @param jObject $div
+ *  The jquery object pointing to the graph div
+ * @param array traces
+ *  An array of trace objects. These objects can hold any option as defined in
+ *  the Plotly.js documentation. 
+ */
 function drawGraph($div, traces, layout, config, post_process = () => {}, register_event = false) {
     let $pending = $div.prev();
     let busy_count = 0;
@@ -93,7 +102,7 @@ function drawGraph($div, traces, layout, config, post_process = () => {}, regist
  *  An array of rows where each row is an object with key value pairs where the
  *  key is the column name.
  * @param object data_source
- *  This object allow sto define how the data from parameter `data` is assigned
+ *  This object allows to define how the data from parameter `data` is assigned
  *  to trace keys. The object has the following keys:
  *  - `name<string>`: the name of the data source table as stored in the DB
  *  - `map<object>`: the association of the data to plotly trace keys. This is
@@ -102,8 +111,8 @@ function drawGraph($div, traces, layout, config, post_process = () => {}, regist
  *    be of the following types:
  *     - `string`: indicating the data column to be used as values
  *     - `array`: an array of operand objects which allow to perform simple
- *       operations on data columns and use the result as values. An operand
- *       object must have the following keys:
+ *       operations on data columns and use the result as values.
+ *       An operand object must have the following keys:
  *        - `name<string>`: the name of the data column
  *        - `op<string>`: one of the following mathematical operations:
  *          - `max`: returns the maximal value of the data column.
@@ -111,26 +120,8 @@ function drawGraph($div, traces, layout, config, post_process = () => {}, regist
  *          - `sum`: returns the sum of all values of the data column.
  *          - `avg`: returns the average of all values of the data column.
  *     - `object`: an object definig how to group values of one data column and
- *       how to assign labels to each group. The object must have the following
- *       keys:
- *       - `name<string>`: the name of the data column.
- *       - `op<string>`: one of the following operations to produce grouping
- *         values:
- *         - `count`: count the occurences of each individual value
- *         - `percent`: count the occurences of each individual value and
- *           divide the result by the rouw count
- *         - `sum`: accumulate distinct values (i.e. count * value)
- *       - `labels<object>`: allows to assign labels to each value. The
- *         following keys are expected:
- *         - `key<string>`: a dot-seperated string which will produce an
- *           element inside the trace object accordingly.
- *         - `map<object>`: a list of key-value pairs where the key corresponds
- *           to the data value to be labelled and the value is a label string.
- *       - `factor<number>`: If defined, this number will be multiplied to each
- *         value computed with the function defined above.
- *       - `offset<number>`: If defined, this number will be added to each
- *         value computed with the function defined above (after multiplying by
- *         `factor`).
+ *       how to assign labels to each group. Refer to graphTraceCbData() for
+ *       more information on the available object keys.
  * @return
  *  A partial plotly trace objects with keys set as defined in param
  *  `data_source`.
@@ -174,6 +165,70 @@ function graphTraceCb(data, data_source) {
     return trace;
 }
 
+/**
+ * This function handles the data processing of the data map value is an object.
+ * When indicated, the function parameter corresponds to a data map object key.
+ *
+ * @param array data
+ *  An array of rows where each row is an object with key value pairs where the
+ *  key is the column name.
+ * @param object trace
+ *  The current graph trace wto work on.
+ * @param string trace_key
+ *  The key of the trace as a dot-notation.
+ * @param string name
+ *  Corresponds to the object map key `name`. It defines the name of the data
+ *  column.
+ * @param array ignore
+ *  Corresponds to the object map key `ignore`. It defines a list of value
+ *  types to ignore when computing the trace.
+ * @param object options
+ *  Corresponds to the object map key `options`. It allows to define
+ *  post-process operations on the trace data. The following keys are available:
+ *   - `op<string>`: one of the following operations to produce grouping
+ *     values:
+ *      - `count`: count the occurences of each individual value
+ *      - `idx`: increment per new value type
+ *      - `percent`: count the occurences of each individual value and
+ *        divide the result by the rouw count
+ *      - `sum`: accumulate distinct values (i.e. count * value)
+ *      - `val`: use the value types
+ *   - `range<object>`: allows to define a range to which the trace values will
+ *     be evenly distributed. The range object must contain the keys `min`
+ *     and `max` to define the minimum and maximum range value, respectively.
+ *   - `factor<number>`: If defined, this number will be multiplied to each
+ *     value computed with the function defined in `op`.
+ *   - `offset<number>`: If defined, this number will be added to each
+ *     value computed with the function defined in `op` (after multiplying by
+ *     `factor`).
+ *   - `round<number>`: Defines the number of digits after the comma to round
+ *     to. 0 means rounding to an integer.
+ *   - `suffix<string>`: Allows to add a suffix to each value.
+ * @param mixed order
+ *  Corresponds to the object map key `order`. This defines the trace order and
+ *  can be of the following types:
+ *   - `string`: either "asc" or "desc" to order the traces either by
+ *     ascending values or by descending values, respectively
+ *   - `array`: an array of indices indication the new position of the
+ *     traces.
+ *   - `object`: an object with the key `vals` which holds an ordered
+ *     list of value types by which the traces will be ordered.
+ * @param object maps
+ *  Corresponds to the object map key `map`. This allows to map static data to
+ *  predefined keys by respecting the post-process options defined above. The
+ *  keys of the object are dot-seperated strings which will produce an element
+ *  inside the trace object accordingly. The values are map objects which will
+ *  map static data to a value type. To use this for labelling value types, the
+ *  key would be the value type and the value the label to assign to the value
+ *  type.
+ * @param object children
+ *  Corresponds to the object map key `children`. This allows to map furter
+ *  data items from the data table to trace keys by respecting the post-process
+ *  options defined above. Children are defined by a value-key pair where the
+ *  key is a dot-seperated string which will produce an element inside the
+ *  trace object accordingly and the value is an option object as defined by
+ *  the parameter `options`.
+ */
 function graphTraceCbData(data, trace, trace_key, name, ignore, options, order, maps = {}, children = {}) {
     let vals = {};
     let trace_opt_keys = {};
@@ -183,6 +238,15 @@ function graphTraceCbData(data, trace, trace_key, name, ignore, options, order, 
     }
     let new_val = false;
     let idx = 0;
+    let has_order = false;
+    let sort_indices = null;
+    console.log(name);
+    console.log(order);
+    if(typeof order === 'object' && order !== null && 'vals' in order) {
+        console.log(true);
+        console.log(order);
+        has_order = true;
+    }
     data.forEach(function(item) {
         let val = item[name];
         if(ignore && ignore.includes(val)) {
@@ -190,10 +254,14 @@ function graphTraceCbData(data, trace, trace_key, name, ignore, options, order, 
         }
         if(!(val in vals)) {
             new_val = true;
-            vals[val] = trace_key.length;
-            trace_key.push(0);
+            if(has_order) {
+                vals[val] = order.vals.indexOf(Number(val));
+            } else {
+                vals[val] = trace_key.length;
+            }
+            trace_key[vals[val]] = 0;
             for(let key in maps) {
-                trace_opt_keys[key].push(maps[key][val]);
+                trace_opt_keys[key][vals[val]] = maps[key][val];
             }
         }
         if(options.op === "count" || options.op === "percent") {
@@ -241,7 +309,6 @@ function graphTraceCbData(data, trace, trace_key, name, ignore, options, order, 
                 + Number.EPSILON) * digit) / digit;
         }
     }
-    let sort_indices = null;
     if(Array.isArray(order)) {
         if(options.op !== "idx") {
             // order by indices but not if its the idx operation
@@ -250,7 +317,7 @@ function graphTraceCbData(data, trace, trace_key, name, ignore, options, order, 
                 trace_key[i] = tmp[order[i]];
             }
         }
-    } else if (order) {
+    } else if (order && typeof order === "string") {
         if(order === "asc") {
             sort_indices = sortWithIndices(trace_key);
         } else if(order === "desc") {
@@ -276,6 +343,16 @@ function graphTraceCbData(data, trace, trace_key, name, ignore, options, order, 
     }
 }
 
+/**
+ * Sorts an array and provides an array which indicates the sort pattern
+ *
+ * @param array toSort
+ *  The array to be sorted
+ * @param boolean asc
+ *  True if sorting should be ascending, False if sorting should be descending
+ * @return
+ *  An array which indicates to new index of each item in the original array.
+ */
 function sortWithIndices(toSort, asc = true) {
     let sortIndices = [];
     let sign = asc ? 1 : -1;
@@ -292,6 +369,18 @@ function sortWithIndices(toSort, asc = true) {
     return sortIndices;
 }
 
+/**
+ * Allows to expand a dot-string into an element reference of an object.
+ *
+ * @param object obj
+ *  The object to parse for the dot-string
+ * @param string str
+ *  The dot-string
+ * @param mixed value
+ *  The default value to assign to the object element
+ * @return
+ *  A reference to the object element.
+ */
 function graphExpandDotString(obj, str, value) {
     var items = str.split('.');
     var ref = obj;
@@ -309,7 +398,16 @@ function graphExpandDotString(obj, str, value) {
     return ref;
 }
 
-
+/**
+ * Parse the JSON data definig a graph. On failure the function will log the
+ * error message to the console.
+ *
+ * @param jObject $data
+ *  A reference to a jQuery object pointing to the HTML element holding the
+ *  JSON string.
+ * @return
+ *  The JSON object on success or null on failure.
+ */
 function parseGraphData($data) {
     var j_data = $data.text();
     try {
