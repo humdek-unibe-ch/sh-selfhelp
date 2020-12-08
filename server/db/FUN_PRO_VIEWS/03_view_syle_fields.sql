f0f705021a5b7a0801b93e9993db56dbb52a244b
Simon Maurer <moirelein@gmail.com>
2020-12-08 16:08:51 +0100

    remove hard-coded css field logic from the code

--- server/db/FUN_PRO_VIEWS/03_view_syle_fields.sql
+++ server/db/FUN_PRO_VIEWS/03_view_syle_fields.sql
@@ -4,5 +4,5 @@ as
 select s.style_id, s.style_name, s.style_type, s.style_group, f.field_id, f.field_name, f.field_type, f.display, f.position, 
 sf.default_value, sf.help
 from view_styles s
-left join styles_fields sf on (s.style_id = sf.id_styles or sf.id_fields = (SELECT id FROM fields WHERE name = 'css')) -- hardcoded for css field
+left join styles_fields sf on (s.style_id = sf.id_styles)
 left join view_fields f on (f.field_id = sf.id_fields);
