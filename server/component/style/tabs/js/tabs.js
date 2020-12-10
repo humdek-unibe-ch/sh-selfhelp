64cdf02b088f047f5d0886cf1f458bc630f6a11c
Simon Maurer <moirelein@gmail.com>
2020-12-10 14:35:58 +0100

    allow to jump to tab anchors

--- server/component/style/tabs/js/tabs.js
+++ server/component/style/tabs/js/tabs.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
-    $('.tabs-container').find('.tab-content').each(function() {
-        $(this).closest('.tabs-container').append($(this));
-    })
-    $('button.tab-button').click(function() {
-        var selector = '.tab-content-index-' + $(this).attr('data-context');
-        var $tab_container = $(this).closest('.tabs-container').children(selector);
-        var $buttons = $(this).closest('.tabs-container').children('button.tab-button');
-        if($(this).hasClass("active"))
+    function activate($this) {
+        var selector = '.tab-content-index-' + $this.attr('data-context');
+        var $tab_container = $this.closest('.tabs-container').children(selector);
+        var $buttons = $this.closest('.tabs-container').children('button.tab-button');
+        if($this.hasClass("active"))
         {
-            $(this).removeClass("active");
+            $this.removeClass("active");
             $tab_container.slideUp("fast");
         }
         else
         {
-            $(this).addClass("active");
+            $this.addClass("active");
             $tab_container.slideDown("fast");
         }
-        $buttons.not($(this)).removeClass("active");
-        $(this).closest('.tabs-container').children('.tab-content').not(selector).hide();
+        $buttons.not($this).removeClass("active");
+        $this.closest('.tabs-container').children('.tab-content').not(selector).hide();
+    }
+    $('.tabs-container').find('.tab-content').each(function() {
+        $(this).closest('.tabs-container').append($(this));
+    });
+    $('button.tab-button.style-' + location.hash.substring(1)).each(function() {
+        activate($(this));
+    });
+    $('button.tab-button').click(function() {
+        activate($(this));
     });
 });
