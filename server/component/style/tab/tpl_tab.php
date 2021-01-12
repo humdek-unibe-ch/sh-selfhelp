db5481c8ee704f58722dbc30d120aa593f55deff
Stefan Kodzhabashev <stefan.kodzhabashev@psy.unibe.ch>
2021-01-12 09:24:55 +0100

    fix syntax for btn type in tab button

--- server/component/style/tab/tpl_tab.php
+++ server/component/style/tab/tpl_tab.php
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 ?>
-<button class="btn btn-<?php echo $this->type; ?> tab-button <?php echo $active; ?> <?php echo $this->css; ?>" data-context="<?php echo $this->id; ?>" type=button>
+<button class="btn btn-<?php echo $this->type; ?> tab-button <?php echo $active; ?> <?php echo $this->css; ?>" data-context="<?php echo $this->id; ?>" type="button">
     <?php echo $this->label; ?>
 </button>
 <div class="card card-body bg-light tab-content tab-content-index-<?php echo $this->id; ?> <?php echo $active; ?>">
