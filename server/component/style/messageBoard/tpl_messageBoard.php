c67596ed16d1c38a98a01c56e6adafc3861fe100
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2022-06-10 16:46:00 +0200

    #314 - fix messageBoard to work with new CMS

--- server/component/style/messageBoard/tpl_messageBoard.php
+++ server/component/style/messageBoard/tpl_messageBoard.php
@@ -1,3 +1,9 @@
+<div class="section-children-ui-cms">
+        <?php if (isset($this->model->get_params()['title'])) {
+            // if we have that parameter we load from cms, show the children
+            $this->output_children();
+        } ?>
+    </div>
 <div class="<?php echo $this->css; ?>">
     <?php $this->output_messages(); ?>
 </div>
