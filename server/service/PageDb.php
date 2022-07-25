d87e33dfe7132a54b86ca55e72a7e042a00aaa73
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2022-07-25 09:53:43 +0200

    fix function creation

--- server/service/PageDb.php
+++ server/service/PageDb.php
@@ -30,7 +30,7 @@ class PageDb extends BaseDb
      */
     function __construct($server, $dbname, $username, $password ) {
         parent::__construct( $server, $dbname, $username, $password );
-        $this->cache->clear_cache();
+        // $this->cache->clear_cache();
         // $this->cache->clear_cache($this->cache::CACHE_TYPE_PAGES, 80);
     }
 
