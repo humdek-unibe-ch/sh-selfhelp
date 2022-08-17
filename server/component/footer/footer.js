19871b6754ef788af97f84dc38a19070d2f5cfc3
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2022-08-17 16:23:59 +0200

    remove some console.log files

--- server/component/footer/footer.js
+++ server/component/footer/footer.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
 
     $('#defaultLanguage select').on('change', function () {
         var id_languages = $(this).val();
-        console.log(id_languages);
         $.post(
             BASE_PATH + '/request/AjaxLanguage/ajax_set_user_language',
             { id_languages: id_languages},
--- server/component/style/radio/js/radio.js
+++ server/component/style/radio/js/radio.js
@@ -4,7 +4,6 @@ $(document).ready(() => {
 
 function check_radio_locked_after_submit(){
     $('.selfhelpRadio').each(function(){
-        console.log( );
         if($(this).data('locked_after_submit') && $('input[name="'+$(this).attr('name')+'"]:checked').val()){
             $('input[name="'+$(this).attr('name')+'"]:not(:checked)').prop('disabled', true);
         }        
