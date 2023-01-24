293db178ca24584f91442b2a0003efacb8385bc6
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2023-01-24 12:05:56 +0100

    add db function dd_foreign_key;

--- server/db/update_scripts/12_update_v5.10.0_v5.11.0.sql
+++ server/db/update_scripts/12_update_v5.10.0_v5.11.0.sql
@@ -53,3 +53,28 @@ UNION
 SELECT 'static' AS `type`, id AS id, `name` AS orig_name, CONCAT(`name`, '_static') AS `table_name`, CONCAT(FLOOR(id),"-","static") AS form_id_plus_type, 0  AS internal
 FROM uploadTables;
 
+DELIMITER //
+DROP PROCEDURE IF EXISTS add_foreign_key //
+CREATE PROCEDURE add_foreign_key(param_table VARCHAR(100), fk_name VARCHAR(100), fk_column VARCHAR(100), fk_references VARCHAR(200))
+BEGIN	
+    SET @sqlstmt = (SELECT IF(
+		(
+			SELECT COUNT(*)
+            FROM information_schema.TABLE_CONSTRAINTS 
+			WHERE `table_schema` = DATABASE()
+			AND `table_name` = param_table
+            AND `constraint_name` = fk_name
+		) > 0,
+        "SELECT 'The foreign key already exists in the table'",
+        CONCAT('ALTER TABLE ', param_table, ' ADD CONSTRAINT ', fk_name, ' FOREIGN KEY (', fk_column, ') REFERENCES ', fk_references, ' ON DELETE CASCADE ON UPDATE CASCADE;')
+    ));
+	PREPARE st FROM @sqlstmt;
+	EXECUTE st;
+	DEALLOCATE PREPARE st;	
+END
+
+//
+
+DELIMITER ;
+
+
