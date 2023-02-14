abf1a5adc06bc11546f368a2c018d689e739e5c1
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2023-02-14 14:12:36 +0100

    add drop_foreign_key sql procedure in the update script

--- server/db/update_scripts/13_update_v5.10.0_v5.11.0.sql
+++ server/db/update_scripts/13_update_v5.10.0_v5.11.0.sql
@@ -107,4 +107,27 @@ END
 
 DELIMITER ;
 
+DELIMITER //
+DROP PROCEDURE IF EXISTS drop_foreign_key //
+CREATE PROCEDURE drop_foreign_key(param_table VARCHAR(100), fk_name VARCHAR(100))
+BEGIN	
+    SET @sqlstmt = (SELECT IF(
+		(
+			SELECT COUNT(*)
+            FROM information_schema.TABLE_CONSTRAINTS 
+			WHERE `table_schema` = DATABASE()
+			AND `table_name` = param_table
+            AND `constraint_name` = fk_name
+		) = 0,
+        "SELECT 'Foreign key does not exist'",
+        CONCAT('ALTER TABLE ', param_table, ' DROP FOREIGN KEY ', fk_name, ' ;')
+    ));
+	PREPARE st FROM @sqlstmt;
+	EXECUTE st;
+	DEALLOCATE PREPARE st;	
+END
+
+//
+
+DELIMITER ;
 
