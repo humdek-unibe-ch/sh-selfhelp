ad4f4c158477c89a7146a71432383c3955022f96
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2023-02-27 11:42:21 +0100

    add domain to the cookies

--- server/service/Login.php
+++ server/service/Login.php
@@ -86,12 +86,13 @@ class Login
         } else if (!isset($_POST['mobile']) || !$_POST['mobile']) {
             // web calls only
             if (PHP_VERSION_ID < 70300) {
-                session_set_cookie_params(6000, '/; samesite=' . 'strict', null, true);
+                session_set_cookie_params(6000, '/; samesite=' . 'strict', $_SERVER['HTTP_HOST'], true);
             } else {
                 session_set_cookie_params(
                     [
                         'secure' => true,
-                        'samesite' => 'strict'
+                        'samesite' => 'strict',
+                        'domain' => $_SERVER['HTTP_HOST']
                     ]
                 );
             }
