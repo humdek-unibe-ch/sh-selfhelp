7c0f74cd0fdd225537bfcec3174749611737bd99
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2023-03-09 09:45:15 +0100

    dynamically load target groups

--- server/component/style/jobConfig/js/jobConfig.js
+++ server/component/style/jobConfig/js/jobConfig.js
@@ -1,7 +1,45 @@
+var editor = null;
+
 $(document).ready(function () {
     loadJobConfig();
 });
 
+
+function createJSONEditor(schema) {
+    if (editor) {
+        editor.destroy();
+    }
+    editor = new JSONEditor($('#jobConfig')[0], {
+        theme: 'bootstrap4',
+        iconlib: 'fontawesome5',
+        ajax: true,
+        schema: schema,
+        show_errors: "always",
+    });
+    editor.on('ready', () => {
+        crrValue = false;
+        try {
+            crrValue = JSON.parse($('#jobConfigValue').val());
+        } catch (error) {
+            // no value is set
+        }
+        if (crrValue) {
+            editor.editors.root.setValue(crrValue, true)
+        }
+        editor.on('change', () => {
+            $('#jobConfig').find('select').each(function () {
+                $(this).selectpicker();
+                $(this).selectpicker('refresh');
+            });
+            console.log(editor.getValue());
+            $('#jobConfigValue').val(JSON.stringify(editor.getValue()));
+        });
+        if ($('#jobConfig').hasClass('view-select')) {
+            // disable the form if we are in view mode
+            editor.disable();
+        }
+    });
+}
 
 /**
  * Load the job config with json-editor library
@@ -17,37 +55,8 @@ function loadJobConfig() {
             dataType: "json",
             url: schemaUrl,
             success: (retrievedSchema) => {
-                editor = new JSONEditor($('#jobConfig')[0], {
-                    theme: 'bootstrap4',
-                    iconlib: 'fontawesome5',
-                    ajax: true,
-                    schema: retrievedSchema,
-                    show_errors: "always",
-                });
-                setDynamicEnums(editor);
-                editor.on('ready', () => {
-                    crrValue = false;
-                    try {
-                        crrValue = JSON.parse($('#jobConfigValue').val());
-                    } catch (error) {
-                        // no value is set
-                    }
-                    if (crrValue) {
-                        editor.editors.root.setValue(crrValue, true)
-                    }
-                    editor.on('change', () => {
-                        $('#jobConfig').find('select').each(function () {
-                            $(this).selectpicker();
-                            $(this).selectpicker('refresh');
-                        });
-                        console.log(editor.getValue());
-                        $('#jobConfigValue').val(JSON.stringify(editor.getValue()));
-                    });
-                    if ($('#jobConfig').hasClass('view-select')) {
-                        // disable the form if we are in view mode
-                        editor.disable();
-                    }
-                })
+                createJSONEditor(retrievedSchema);
+                setDynamicEnums();
             }
         });
     }
@@ -71,9 +80,13 @@ function prepareEnumSource(values) {
     return res;
 }
 
-async function setDynamicEnums(editor) {
+async function setDynamicEnums() {
     var groups = await getGroups();
     editor.schema.definitions.job_ref.properties.job_add_remove_groups.items.enum = groups;
+    console.log(editor.schema.properties.selected_target_groups.items.enum);
+    editor.schema.properties.selected_target_groups.items.enum = [...groups];
+    console.log(JSON.stringify(editor.schema.properties.selected_target_groups.items.enum));
     var actionScheduleTypes = await getLookups('actionScheduleTypes');
     editor.schema.definitions.schedule_time_ref.properties.job_schedule_types.enumSource = prepareEnumSource(actionScheduleTypes);
+    createJSONEditor(editor.schema); // after changes the forms should be recreated
 }
