86de06ec780a03fb927ff1019d3a201e04014262
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2023-03-17 11:53:25 +0100

    rename sql version to 6.0.0

--- server/db/update_scripts/14_update_v5.11.0_v6.0.0.sql
+++ server/db/update_scripts/14_update_v5.11.0_v6.0.0.sql
@@ -1,6 +1,6 @@
 -- set DB version
 UPDATE version
-SET version = 'v5.12.0';
+SET version = 'v6.0.0';
 
 UPDATE sections_fields_translation
 SET content = REPLACE(content, '"type": "static"', '"type": "EXTERNAL"')
