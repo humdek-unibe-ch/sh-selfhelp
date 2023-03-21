6f57cc02f9d364fc4c998a8775bab1075a79dc7e
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2023-03-21 14:02:51 +0100

    add css to the conditionBuilder style

--- server/component/style/conditionBuilder/tpl_condition_builder.php
+++ server/component/style/conditionBuilder/tpl_condition_builder.php
@@ -3,5 +3,5 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 ?>
-<button type="button" class="w-100 mt-1 btn conditionBuilderBtn btn-sm <?php echo $button_label == 'Edit Condition' ? 'btn-warning' : 'btn-primary'; ?>"><?php echo $button_label; ?></button>
+<button type="button" class="w-100 mt-1 btn conditionBuilderBtn btn-sm <?php echo $this->css; ?> <?php echo $button_label == 'Edit Condition' ? 'btn-warning' : 'btn-primary'; ?>"><?php echo $button_label; ?></button>
 <div id="builder"></div>
