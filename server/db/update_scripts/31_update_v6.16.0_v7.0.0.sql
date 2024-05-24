bd7bf5d91815e239df6eb9f820b50ede7d8b811b
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2024-05-24 16:15:33 +0200

    move user input refactor in a precedure and do it only once

--- server/db/update_scripts/31_update_v6.16.0_v7.0.0.sql
+++ server/db/update_scripts/31_update_v6.16.0_v7.0.0.sql
@@ -17,46 +17,75 @@ WHERE type_code = 'actionTriggerTypes' AND lookup_code = 'finished';
 CALL add_table_column('uploadRows', 'id_actionTriggerTypes', "int(10) unsigned zerofill DEFAULT NULL");
 CALL add_foreign_key('uploadRows', 'uploadRows_fk_id_actionTriggerTypes', 'id_actionTriggerTypes', 'lookups (id)');
 
-CALL add_table_column('uploadRows', 'old_row_id', "int(10) unsigned zerofill DEFAULT NULL");
-CALL add_table_column('uploadCols', 'old_col_id', "int(10) unsigned zerofill DEFAULT NULL");
 
-INSERT INTO uploadTables (`name`)
-SELECT DISTINCT CAST(id_sections AS CHAR) AS `name`
-FROM user_input_record WHERE id_sections > 0;
+DELIMITER //
+CREATE PROCEDURE refactor_user_input()
+BEGIN
+	
+    DECLARE table_exists INT;
+    
+    -- Check if the table exists
+    SELECT COUNT(*)
+    INTO table_exists
+    FROM information_schema.tables
+    WHERE table_schema = DATABASE() AND `table_name` = 'user_input';
+    
+	IF table_exists > 0 THEN
 
-INSERT INTO uploadRows (id_uploadTables, `timestamp`, id_users, old_row_id)
-SELECT DISTINCT ut.id, uir.create_time, id_users, uir.id
-FROM user_input_record uir
-JOIN uploadTables ut ON ut.`name` = CAST(uir.id_sections AS CHAR)
-JOIN user_input ui ON ui.id_user_input_record = uir.id
-WHERE uir.id_sections > 0;
+		CALL add_table_column('uploadRows', 'old_row_id', "int(10) unsigned zerofill DEFAULT NULL");
+		CALL add_table_column('uploadCols', 'old_col_id', "int(10) unsigned zerofill DEFAULT NULL");
 
-INSERT INTO uploadCols (`name`, id_uploadTables, old_col_id)
-SELECT DISTINCT sft_in.content AS `name`, ut.id, ui.id_sections
-FROM uploadTables ut
-JOIN user_input_record uir ON CAST(uir.id_sections AS CHAR) = ut.`name`
-JOIN user_input ui ON ui.id_user_input_record = uir.id
-JOIN sections_fields_translation AS sft_in ON sft_in.id_sections = ui.id_sections AND sft_in.id_fields = 57;
+		INSERT INTO uploadTables (`name`)
+		SELECT DISTINCT CAST(id_sections AS CHAR) AS `name`
+		FROM user_input_record WHERE id_sections > 0;
 
-INSERT IGNORE INTO uploadCells (id_uploadRows, id_uploadCols, `value`)
-SELECT DISTINCT ur.id, uc.id, ui.`value`
-FROM user_input ui
-JOIN uploadRows ur ON (ui.id_user_input_record = ur.old_row_id)
-JOIN uploadCols uc ON uc.old_col_id = ui.id_sections;
+		INSERT INTO uploadRows (id_uploadTables, `timestamp`, id_users, old_row_id)
+		SELECT DISTINCT ut.id, uir.create_time, id_users, uir.id
+		FROM user_input_record uir
+		JOIN uploadTables ut ON ut.`name` = CAST(uir.id_sections AS CHAR)
+		JOIN user_input ui ON ui.id_user_input_record = uir.id
+		WHERE uir.id_sections > 0;
 
--- add trigger deleted to the removed entries
-UPDATE uploadRows ur
-JOIN (
-    SELECT 
-        ui.id_user_input_record,
-        CASE 
-            WHEN ui.removed = 1 THEN (SELECT id FROM lookups WHERE type_code = 'actionTriggerTypes' AND lookup_code = 'deleted' )
-            ELSE NULL 
-        END AS removed
-    FROM user_input ui
-) subquery ON ur.old_row_id = subquery.id_user_input_record
-SET ur.id_actionTriggerTypes = subquery.removed
-WHERE ur.old_row_id > 0;
+		INSERT INTO uploadCols (`name`, id_uploadTables, old_col_id)
+		SELECT DISTINCT sft_in.content AS `name`, ut.id, ui.id_sections
+		FROM uploadTables ut
+		JOIN user_input_record uir ON CAST(uir.id_sections AS CHAR) = ut.`name`
+		JOIN user_input ui ON ui.id_user_input_record = uir.id
+		JOIN sections_fields_translation AS sft_in ON sft_in.id_sections = ui.id_sections AND sft_in.id_fields = 57;
 
-CALL drop_table_column('uploadRows', 'old_row_id');
-CALL drop_table_column('uploadCols', 'old_col_id');
+		INSERT IGNORE INTO uploadCells (id_uploadRows, id_uploadCols, `value`)
+		SELECT DISTINCT ur.id, uc.id, ui.`value`
+		FROM user_input ui
+		JOIN uploadRows ur ON (ui.id_user_input_record = ur.old_row_id)
+		JOIN uploadCols uc ON uc.old_col_id = ui.id_sections;
+
+		-- add trigger deleted to the removed entries
+		UPDATE uploadRows ur
+		JOIN (
+			SELECT 
+				ui.id_user_input_record,
+				CASE 
+					WHEN ui.removed = 1 THEN (SELECT id FROM lookups WHERE type_code = 'actionTriggerTypes' AND lookup_code = 'deleted' )
+					ELSE NULL 
+				END AS removed
+			FROM user_input ui
+		) subquery ON ur.old_row_id = subquery.id_user_input_record
+		SET ur.id_actionTriggerTypes = subquery.removed
+		WHERE ur.old_row_id > 0;
+
+		CALL drop_table_column('uploadRows', 'old_row_id');
+		CALL drop_table_column('uploadCols', 'old_col_id');
+
+		RENAME TABLE user_input TO deprecated_user_input;
+		RENAME TABLE user_input_record TO deprecated_user_input_record;
+	ELSE
+		SELECT 'User input is already refactored' AS message;
+	END IF;	 
+     
+ END //
+
+DELIMITER ;
+
+CALL refactor_user_input();
+
+DROP PROCEDURE IF EXISTS refactor_user_input;
