67ea6a4ec936d31f0951f5cafe43455cec06f34c
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2024-06-07 13:13:52 +0200

    refactor iew_mailQueue;

--- server/db/FUN_PRO_VIEWS/26_view_mailQueue.sql
+++ server/db/FUN_PRO_VIEWS/26_view_mailQueue.sql
@@ -5,7 +5,7 @@ SELECT sj.id AS id, from_email, from_name,
 status_code, `status`, type_code, `type`, 
 sj.date_create, date_to_be_executed, date_executed,
 reply_to, recipient_emails, cc_emails, bcc_emails, `subject`, body, is_html, mq.id AS id_mailQueue, id_jobTypes,
-id_jobStatus, sj.config, id_user_input_record, id_uploadRows, internal_table, external_table
+id_jobStatus, sj.config, id_dataRows, dataTables_name
 FROM mailQueue mq
 INNER JOIN scheduledJobs_mailQueue sj_mq ON (sj_mq.id_mailQueue = mq.id)
 INNER JOIN view_scheduledJobs sj ON (sj.id = sj_mq.id_scheduledJobs);
--- server/db/update_scripts/31_update_v6.16.0_v7.0.0.sql
+++ server/db/update_scripts/31_update_v6.16.0_v7.0.0.sql
@@ -325,6 +325,54 @@ FROM formActions fa
 INNER JOIN lookups trig ON (trig.id = fa.id_formProjectActionTriggerTypes)
 LEFT JOIN view_dataTables dt ON (dt.id = fa.id_dataTables);
 
+DROP VIEW IF EXISTS view_scheduledJobs;
+CREATE VIEW view_scheduledJobs
+AS
+SELECT sj.id AS id, l_status.lookup_code AS status_code, l_status.lookup_value AS `status`, 
+l_types.lookup_code AS type_code, l_types.lookup_value AS `type`, sj.config,
+sj.date_create, date_to_be_executed, date_executed, `description`, 
+CASE
+	WHEN l_types.lookup_code = 'email' THEN mq.recipient_emails    
+    WHEN l_types.lookup_code = 'notification' THEN ''
+    WHEN l_types.lookup_code = 'task' THEN ''
+    ELSE ""
+END AS recipient,
+CASE
+	WHEN l_types.lookup_code = 'email' THEN mq.`subject`
+    WHEN l_types.lookup_code = 'notification' THEN n.`subject`
+    ELSE ""
+END AS title,
+CASE
+	WHEN l_types.lookup_code = 'email' THEN mq.body
+    WHEN l_types.lookup_code = 'notification' THEN n.body
+    ELSE ""
+END AS message,
+sj_mq.id_mailQueue, id_jobTypes, id_jobStatus, a.id_formActions,
+a.id_dataRows, dt.`name` AS dataTables_name
+FROM scheduledJobs sj
+INNER JOIN lookups l_status ON (l_status.id = sj.id_jobStatus)
+INNER JOIN lookups l_types ON (l_types.id = sj.id_jobTypes)
+LEFT JOIN scheduledJobs_mailQueue sj_mq ON (sj_mq.id_scheduledJobs = sj.id)
+LEFT JOIN mailQueue mq ON (mq.id = sj_mq.id_mailQueue)
+LEFT JOIN scheduledJobs_notifications sj_n ON (sj_n.id_scheduledJobs = sj.id)
+LEFT JOIN notifications n ON (n.id = sj_n.id_notifications)
+LEFT JOIN scheduledJobs_formActions a ON (a.id_scheduledJobs = sj.id)
+LEFT JOIN dataRows r ON (r.id = a.id_dataRows)
+LEFT JOIN view_dataTables dt ON (r.id_dataTables = dt.id);
+
+DROP VIEW IF EXISTS view_mailQueue;
+CREATE VIEW view_mailQueue
+AS
+SELECT sj.id AS id, from_email, from_name,
+status_code, `status`, type_code, `type`, 
+sj.date_create, date_to_be_executed, date_executed,
+reply_to, recipient_emails, cc_emails, bcc_emails, `subject`, body, is_html, mq.id AS id_mailQueue, id_jobTypes,
+id_jobStatus, sj.config, id_dataRows, dataTables_name
+FROM mailQueue mq
+INNER JOIN scheduledJobs_mailQueue sj_mq ON (sj_mq.id_mailQueue = mq.id)
+INNER JOIN view_scheduledJobs sj ON (sj.id = sj_mq.id_scheduledJobs);
+
+
 
 -- remove the delete option from the form; create a new style for deleteing form record
 
