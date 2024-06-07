1ddf290cb098e8110f17c8da2f6f1984b71367e9
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2024-06-07 09:56:48 +0200

    add drop_indesx sql procedure

---
+++ server/db/FUN_PRO_VIEWS/01_drop_index.sql
@@ -0,0 +1,23 @@
+DELIMITER //
+DROP PROCEDURE IF EXISTS drop_index //
+CREATE PROCEDURE drop_index(param_table VARCHAR(100), param_index_name VARCHAR(100))
+BEGIN	
+    SET @sqlstmt = (SELECT IF(
+		(
+			SELECT COUNT(*)
+            FROM information_schema.STATISTICS 
+			WHERE `table_schema` = DATABASE()
+			AND `table_name` = param_table
+            AND `index_name` = param_index_name
+		) > 0,        
+        CONCAT('ALTER TABLE ', param_table, ' DROP INDEX ', param_index_name),
+        "SELECT 'The index does not exists in the table'"
+    ));
+	PREPARE st FROM @sqlstmt;
+	EXECUTE st;
+	DEALLOCATE PREPARE st;	
+END
+
+//
+
+DELIMITER ;
