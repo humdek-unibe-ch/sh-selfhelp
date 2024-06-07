4ca109f851761ec16fee4127ebdd3d8e60fcc99a
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2024-06-07 13:14:52 +0200

    refactor iew_notifications;

--- server/db/FUN_PRO_VIEWS/28_view_notifications.sql
+++ server/db/FUN_PRO_VIEWS/28_view_notifications.sql
@@ -5,7 +5,7 @@ SELECT sj.id AS id,
 status_code, `status`, type_code, `type`, 
 sj.date_create, date_to_be_executed, date_executed,
 recipient, `subject`, body, url, id_notifications, id_jobTypes,
-id_jobStatus, sj.config, id_user_input_record, id_uploadRows, internal_table, external_table
+id_jobStatus, sj.config, id_dataRows, dataTables_name
 FROM notifications n
 INNER JOIN scheduledJobs_notifications sj_n ON (sj_n.id_notifications = n.id)
 INNER JOIN view_scheduledJobs sj ON (sj.id = sj_n.id_scheduledJobs);
--- server/db/update_scripts/31_update_v6.16.0_v7.0.0.sql
+++ server/db/update_scripts/31_update_v6.16.0_v7.0.0.sql
@@ -372,6 +372,19 @@ FROM mailQueue mq
 INNER JOIN scheduledJobs_mailQueue sj_mq ON (sj_mq.id_mailQueue = mq.id)
 INNER JOIN view_scheduledJobs sj ON (sj.id = sj_mq.id_scheduledJobs);
 
+DROP VIEW IF EXISTS view_notifications;
+CREATE VIEW view_notifications
+AS
+SELECT sj.id AS id,
+status_code, `status`, type_code, `type`, 
+sj.date_create, date_to_be_executed, date_executed,
+recipient, `subject`, body, url, id_notifications, id_jobTypes,
+id_jobStatus, sj.config, id_dataRows, dataTables_name
+FROM notifications n
+INNER JOIN scheduledJobs_notifications sj_n ON (sj_n.id_notifications = n.id)
+INNER JOIN view_scheduledJobs sj ON (sj.id = sj_n.id_scheduledJobs);
+
+
 
 
 -- remove the delete option from the form; create a new style for deleteing form record
