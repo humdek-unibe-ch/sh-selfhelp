5df3ef94dd301f25763112bab9913d3d1df5d480
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2024-06-07 12:35:11 +0200

    refactor iew_formActions;

--- server/db/update_scripts/31_update_v6.16.0_v7.0.0.sql
+++ server/db/update_scripts/31_update_v6.16.0_v7.0.0.sql
@@ -306,6 +306,17 @@ CASE
 END AS `text` -- used for slect dropdowns
 FROM dataTables;
 
+DROP VIEW IF EXISTS view_formActions;
+CREATE VIEW view_formActions
+AS
+SELECT fa.id AS id, fa.`name` AS action_name, dt.`name` AS dataTable_name,
+fa.id_formProjectActionTriggerTypes, trig.lookup_value AS trigger_type, trig.lookup_code AS trigger_type_code,
+config,
+dt.id AS id_dataTables
+FROM formActions fa 
+INNER JOIN lookups trig ON (trig.id = fa.id_formProjectActionTriggerTypes)
+LEFT JOIN view_dataTables dt ON (dt.id = fa.id_dataTables);
+
 
 -- remove the delete option from the form; create a new style for deleteing form record
 
