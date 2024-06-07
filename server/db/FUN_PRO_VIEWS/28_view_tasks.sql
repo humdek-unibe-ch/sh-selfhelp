311850600224107bca21cbd043ffcc1b66ae68d2
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2024-06-07 13:15:41 +0200

    refactor iew_tasks;

--- server/db/FUN_PRO_VIEWS/28_view_tasks.sql
+++ server/db/FUN_PRO_VIEWS/28_view_tasks.sql
@@ -4,7 +4,7 @@ AS
 SELECT sj.id AS id,
 status_code, `status`, type_code, `type`, 
 sj.date_create, date_to_be_executed, date_executed,
-recipient, t.config, id_tasks, id_jobTypes, id_jobStatus, `description`, id_user_input_record, id_uploadRows, internal_table, external_table
+recipient, t.config, id_tasks, id_jobTypes, id_jobStatus, `description`, id_dataRows, dataTables_name
 FROM tasks t
 INNER JOIN scheduledJobs_tasks sj_t ON (sj_t.id_tasks = t.id)
 INNER JOIN view_scheduledJobs sj ON (sj.id = sj_t.id_scheduledJobs);
--- server/db/update_scripts/31_update_v6.16.0_v7.0.0.sql
+++ server/db/update_scripts/31_update_v6.16.0_v7.0.0.sql
@@ -384,6 +384,16 @@ FROM notifications n
 INNER JOIN scheduledJobs_notifications sj_n ON (sj_n.id_notifications = n.id)
 INNER JOIN view_scheduledJobs sj ON (sj.id = sj_n.id_scheduledJobs);
 
+DROP VIEW IF EXISTS view_tasks;
+CREATE VIEW view_tasks
+AS
+SELECT sj.id AS id,
+status_code, `status`, type_code, `type`, 
+sj.date_create, date_to_be_executed, date_executed,
+recipient, t.config, id_tasks, id_jobTypes, id_jobStatus, `description`, id_dataRows, dataTables_name
+FROM tasks t
+INNER JOIN scheduledJobs_tasks sj_t ON (sj_t.id_tasks = t.id)
+INNER JOIN view_scheduledJobs sj ON (sj.id = sj_t.id_scheduledJobs);
 
 
 
