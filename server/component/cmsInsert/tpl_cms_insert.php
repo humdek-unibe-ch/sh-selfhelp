beb8416c72a857e9ccce70fe827425a64d776a74
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2024-10-21 11:47:21 +0200

    adjust form lable for bootstrpa 5

--- server/component/cmsInsert/tpl_cms_insert.php
+++ server/component/cmsInsert/tpl_cms_insert.php
@@ -15,40 +15,40 @@
         <div class="card-body">
             <form action="<?php echo $action_url; ?>" method="post">
                 <div class="mb-3">
-                    <label>Keyword</label>
+                    <label class="form-label">Keyword</label>
                     <input type="text" class="form-control" name="keyword" placeholder="Enter keyword" required pattern="<?php echo NAME_PATTERN ?>">
                     <small class="form-text text-body-secondary">The page keyword must be unique, otherwise the page creation will fail. <b>Note that the page keyword can contain numbers, letters, - and _ characters</b></small>                    
                 </div>
                 <div class="mb-3">
                     <div>
-                        <label>Page Type</label>
+                        <label class="form-label">Page Type</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="radio" name="type" value="3" checked>
-                        <label class="form-check-label">Sections</label>
+                        <label class="form-check-label form-label">Sections</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="radio" name="type" value="4">
-                        <label class="form-check-label">Navigation</label>
+                        <label class="form-check-label form-label">Navigation</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="radio" name="type" value="2" disabled>
-                        <label class="form-check-label">Component</label>
+                        <label class="form-check-label form-label">Component</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="radio" name="type" value="1" disabled>
-                        <label class="form-check-label">Backend</label>
+                        <label class="form-check-label form-label">Backend</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="radio" name="type" value="5" disabled>
-                        <label class="form-check-label">Ajax</label>
+                        <label class="form-check-label form-label">Ajax</label>
                     </div>
-                    <small class="form-text text-body-secondary">The page type specified how the page content will be assembled. It is recommended to either use the type <code>Sections</code> or <code>Navigation</code>. Pages of type <code>Component</code> and <code>Custom</code> require PHP programming and cannot be handled by the CMS.</small>
+                    <small class="d-block form-text text-body-secondary">The page type specified how the page content will be assembled. It is recommended to either use the type <code>Sections</code> or <code>Navigation</code>. Pages of type <code>Component</code> and <code>Custom</code> require PHP programming and cannot be handled by the CMS.</small>
                 </div>
-                <div id="header-position" class="mb-3">
+                <div id="header-position" class="mb-3 d-flex flex-column">
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="checkbox" name="set-position" value="<?php echo $this->position_value; ?>">
-                        <label class="form-check-label text-body-secondary">Header Position</label>
+                        <label class="form-check-label text-body-secondary form-label">Header Position</label>
                     </div>
                     <div id="page-order-wrapper" class="d-none">
                     <?php $this->output_page_order(); ?>
@@ -58,48 +58,48 @@
                 <div id="headless-check" class="mb-3 d-none">
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="checkbox" name="set-headless" value="1">
-                        <label class="form-check-label">Headless Page</label>
+                        <label class="form-check-label form-label">Headless Page</label>
                     </div>
                     <small class="form-text text-body-secondary">A headless page will <strong>not</strong> render any header or footer.</small>
                 </div>
                 <div class="mb-3">
-                    <div class="form-check form-check-inline w-100">
+                    <div class="form-check-inline w-100">
                         <?php $this->output_page_access_type(); ?>
                     </div>
                     <small class="form-text text-body-secondary">Page access type: mobile, web or mobile_and_web</small>
                 </div>
                 <div id="protocol-list" class="mb-3 d-none">
                     <div>
-                        <label>Protocol</label>
+                        <label class="form-label">Protocol</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="checkbox" name="protocol[]" value="GET" checked>
-                        <label class="form-check-label">GET</label>
+                        <label class="form-check-label form-label">GET</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="checkbox" name="protocol[]" value="POST" checked>
-                        <label class="form-check-label">POST</label>
+                        <label class="form-check-label form-label">POST</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="checkbox" name="protocol[]" value="PUT">
-                        <label class="form-check-label">PUT</label>
+                        <label class="form-check-label form-label">PUT</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="checkbox" name="protocol[]" value="PATCH">
-                        <label class="form-check-label">PATCH</label>
+                        <label class="form-check-label form-label">PATCH</label>
                     </div>
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="checkbox" name="protocol[]" value="DELETE">
-                        <label class="form-check-label">DELETE</label>
+                        <label class="form-check-label form-label">DELETE</label>
                     </div>
-                    <small class="form-text text-body-secondary">The protocol specifies how a page is accessed. <code>GET</code> is required to display the content of a page and <code>POST</code> is required to send forms to the page. <code>PUT</code>, <code>PATCH</code>, and <code>DELETE</code> may only become necessary for pages of type <code>Component</code> or <code>Custom</code>.</small>
+                    <small class="d-block form-text text-body-secondary">The protocol specifies how a page is accessed. <code>GET</code> is required to display the content of a page and <code>POST</code> is required to send forms to the page. <code>PUT</code>, <code>PATCH</code>, and <code>DELETE</code> may only become necessary for pages of type <code>Component</code> or <code>Custom</code>.</small>
                 </div>
                 <div class="mb-3">
-                    <label>Url Pattern</label>
+                    <label class="form-label">Url Pattern</label>
                     <input type="text" class="form-control" name="url" value="" placeholder="automatic" required readonly>
                     <small class="form-text text-body-secondary">This is set automatically. If you know what you are doing you may overwrite the value. Refer to the documentation of <a href="https://dannyvankooten.github.io/AltoRouter/usage/mapping-routes.html">Altorouter</a> for more information.</small>
                 </div>
-                <div class="mb-3">
+                <div class="mb-3 d-flex flex-column">
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="checkbox" name="set-open" value="1">
                         <label class="form-check-label">Open Access</label>
@@ -109,7 +109,7 @@
                 <div class="mb-3">
                     <div class="form-check form-check-inline">
                         <input class="form-check-input" type="checkbox" name="set-advanced" value="1">
-                        <label class="form-check-label">Advanced</label>
+                        <label class="form-check-label form-label">Advanced</label>
                     </div>
                 </div>
                 <button type="submit" class="btn btn-primary">Create</button>
--- server/component/style/formField/tpl_label.php
+++ server/component/style/formField/tpl_label.php
@@ -4,6 +4,6 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 ?>
 <div class="mb-3 <?php echo $this->css_group; ?> <?php echo $this->css; ?>">
-    <label class="<?php echo $this->css_label; ?>"><?php echo $this->label; ?></label>
+    <label class="form-label <?php echo $this->css_label; ?>"><?php echo $this->label; ?></label>
     <?php $this->output_base_form_field(); ?>
 </div>
