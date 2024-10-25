13f4d866d4aa33403fbfca6ac3aa00fffabc110f
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2024-10-25 11:08:08 +0200

    make the jumbotron display block for BS5

--- server/component/style/jumbotron/tpl_jumbotron.php
+++ server/component/style/jumbotron/tpl_jumbotron.php
@@ -3,6 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 ?>
-<div id="section-<?php echo $this->id_section; ?>" class="card card-header mb-4 rounded-2 py-5 px-3 <?php echo $this->css; ?>">
+<div id="section-<?php echo $this->id_section; ?>" class="card card-header mb-4 rounded-2 py-5 px-3 d-block <?php echo $this->css; ?>">
     <?php $this->output_children(); ?>
 </div>
