4f105223b79e08912851f5b5b0feb9b42810cdaf
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2025-05-13 15:15:18 +0200

    properly catch urls with symphony

--- server/sympony/sh-selfhelp-symfony/src/Controller/Api/V1/Admin/PageSectionController.php
+++ server/sympony/sh-selfhelp-symfony/src/Controller/Api/V1/Admin/PageSectionController.php
@@ -5,7 +5,6 @@ namespace App\Controller\Api\V1\Admin;
 use App\Service\PageService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
--- server/sympony/sh-selfhelp-symfony/src/Controller/Api/V1/AuthController.php
+++ server/sympony/sh-selfhelp-symfony/src/Controller/Api/V1/AuthController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Api\V1\Auth;
+namespace App\Controller\Api\V1;
 
 use App\Entity\User;
 use App\Service\JWTService;
