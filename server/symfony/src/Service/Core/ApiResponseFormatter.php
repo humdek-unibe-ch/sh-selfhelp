e2a43b9adc8f237cb92bd9235f3228ed50287fd3
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2025-06-04 10:20:16 +0200

    use symfony serialzier for data that is not serialzied

--- server/symfony/src/Service/Core/ApiResponseFormatter.php
+++ server/symfony/src/Service/Core/ApiResponseFormatter.php
@@ -9,6 +9,9 @@ use Symfony\Bundle\SecurityBundle\Security;
 use App\Service\JSON\JsonSchemaValidationService;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 /**
  * API response formatter service
@@ -37,6 +40,9 @@ class ApiResponseFormatter
     {
         $isLoggedIn = $isLoggedIn || $this->security->getUser() !== null;
 
+        // Normalize any Doctrine entities in the data using Symfony Serializer
+        $normalizedData = $this->normalizeWithSymfonySerializer($data);
+        
         $responseData = [
             'status' => $status,
             'message' => Response::$statusTexts[$status] ?? 'OK',
@@ -46,7 +52,7 @@ class ApiResponseFormatter
                 'version' => 'v1', // Consider making this configurable
                 'timestamp' => (new \DateTimeImmutable())->format(\DateTimeInterface::ATOM),
             ],
-            'data' => $data,
+            'data' => $normalizedData,
             // validation field is not included in success responses
         ];
 
@@ -219,5 +225,41 @@ class ApiResponseFormatter
         
         // Otherwise, return as is
         return $data;
+    }
+    
+    /**
+     * Normalizes data before sending it in a response using Symfony Serializer
+     * This handles Doctrine entities and converts them to arrays
+     * 
+     * @param mixed $data The data to normalize
+     * @return mixed The normalized data
+     */
+    private function normalizeWithSymfonySerializer($data)
+    {
+        if ($data === null) {
+            return null;
+        }
+        
+        // For simple scalar types, return as is
+        if (is_scalar($data)) {
+            return $data;
+        }
+        
+        // Create a serializer instance with appropriate normalizers and encoders
+        $normalizer = new ObjectNormalizer(null, null, null, null, null, null, [
+            ObjectNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object) {
+                // For circular references, just return the ID if available
+                return method_exists($object, 'getId') ? ['id' => $object->getId()] : null;
+            },
+            ObjectNormalizer::CIRCULAR_REFERENCE_LIMIT => 1,
+            // Ignore Doctrine proxy properties
+            ObjectNormalizer::IGNORED_ATTRIBUTES => ['__initializer__', '__cloner__', '__isInitialized__']
+        ]);
+        
+        $serializer = new Serializer([$normalizer], [new JsonEncoder()]);
+        
+        // First serialize to JSON, then decode back to array
+        $json = $serializer->serialize($data, 'json');
+        return json_decode($json, true);
     }
 }
