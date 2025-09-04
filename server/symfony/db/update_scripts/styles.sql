ff578ac074bd78e4788c09ca17d2826502a8d763
Stefan Kodzhabashev <stefan.kodzhabashev@unibe.ch>
2025-09-04 17:29:33 +0200

    add maninte variant tittle

--- server/symfony/db/update_scripts/styles.sql
+++ server/symfony/db/update_scripts/styles.sql
@@ -2,14 +2,24 @@
 DELETE FROM styles_fields
 WHERE id_fields = get_field_id('type') and id_styles = get_style_id('button');
 
-
+-- strucutre of the config field:
+-- export interface IFieldConfig {
+--     multiSelect?: boolean;
+--     creatable?: boolean;
+--     separator?: string;
+--     options?: Array<{
+--         value: string;
+--         text: string;
+--     }>;
+--     apiUrl?: string;
+-- }
 CALL add_table_column('fields', 'config', 'JSON DEFAULT NULL');
 
 -- Add new field type `select`
 INSERT IGNORE INTO `fieldType` (`id`, `name`, `position`) VALUES (NULL, 'select', '1');
 
 -- Add new field `mantine_variant` from type `select` based on the mantine button variant
-INSERT IGNORE INTO `fields` (`id`, `name`, `id_type`, `display`, `config`) VALUES (NULL, 'mantine_variant', get_field_type_id('select'), '1', '{"options":[{
+INSERT IGNORE INTO `fields` (`id`, `name`, `id_type`, `display`, `config`) VALUES (NULL, 'mantine_variant', get_field_type_id('select'), 0, '{"options":[{
 "value":"filled",
 "text":"Filled"
 },
@@ -39,5 +49,5 @@ INSERT IGNORE INTO `fields` (`id`, `name`, `id_type`, `display`, `config`) VALUE
 }]}');
 
 -- Add new field `mantine_variant` to `button` style
-INSERT IGNORE INTO `styles_fields` (`id_styles`, `id_fields`, `default_value`, `help`, `disabled`, `hidden`) 
-VALUES (get_style_id('button'), get_field_id('mantine_variant'), 'filled', 'Select variant for the button. For more information check https://mantine.dev/core/button', 0, 0);
+INSERT IGNORE INTO `styles_fields` (`id_styles`, `id_fields`, `default_value`, `help`, `disabled`, `hidden`, `title`) 
+VALUES (get_style_id('button'), get_field_id('mantine_variant'), 'filled', 'Select variant for the button. For more information check https://mantine.dev/core/button', 0, 0, 'Mantine Variant');
